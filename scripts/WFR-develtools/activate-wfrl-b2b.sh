b643773c115b00ae4ccf3a36e55159acb0308882
Ira Weiny <ira.weiny@intel.com>
2013-07-30 19:20:09 -0400

    wfr-scripts: b2b make default to not reload drivers

--- scripts/WFR-develtools/activate-wfrl-b2b.sh
+++ scripts/WFR-develtools/activate-wfrl-b2b.sh
@@ -12,6 +12,7 @@ remote_port=2
 remote_lid=2
 
 ibmode="false"
+reload_drivers="false"
 
 function usage
 {
@@ -21,15 +22,16 @@ usage:
 
 Options:
 
--h         - this help text
--p port    - local port to operate on (default $local_port)
--r port    - remote port to operate on (default $remote_port)
--i         - ignore VL15 over VL0 set up (run in default (i)B mode)
--n node    - remote node to operate on
+-h         - h)elp
+-p port    - l)ocal port to operate on (default $local_port)
+-r port    - r)emote port to operate on (default $remote_port)
+-i         - i)gnore VL15 over VL0 set up (run in default (i)B mode)
+-n node    - n)ode remote node to configure
+-d         - d)o reload drivers
 EOL
 }
 
-while getopts "hr:p:n:i" opt; do
+while getopts "hr:p:n:id" opt; do
 	case "$opt" in
 	r)	remote_port="$OPTARG"
 		;;
@@ -38,6 +40,8 @@ while getopts "hr:p:n:i" opt; do
 	n)	remote_node="$OPTARG"
 		;;
 	i)	ibmode="true"
+		;;
+	d)	reload_drivers="true"
 		;;
 	h)	usage
 		exit 0
@@ -54,33 +58,43 @@ if [ "$remote_node" == "" ]; then
 	exit 1
 fi
 
-echo "Reloading ib_wfr_lite on \"$remote_node\""
-ssh root@$remote_node "rmmod ib_qib"
-ssh root@$remote_node "rmmod ib_wfr_lite"
-ssh root@$remote_node "rmmod ib_umad"
-ssh root@$remote_node "rmmod ib_usa"
-ssh root@$remote_node "rmmod ib_sa"
-ssh root@$remote_node "rmmod ib_mad"
-ssh root@$remote_node "rmmod ib_uverbs"
-ssh root@$remote_node "modprobe ib_wfr_lite"
-ssh root@$remote_node "modprobe ib_umad"
-ssh root@$remote_node "modprobe ib_uverbs"
-ssh root@$remote_node "modprobe ib_usa"
-sleep 5
+if [ "$reload_drivers" == "true" ]; then
+	echo "Reloading ib_wfr_lite on \"$remote_node\""
+	ssh root@$remote_node "rmmod ib_qib"
+	ssh root@$remote_node "rmmod ib_wfr_lite"
+	ssh root@$remote_node "rmmod ib_umad"
+	ssh root@$remote_node "rmmod ib_usa"
+	ssh root@$remote_node "rmmod ib_sa"
+	ssh root@$remote_node "rmmod ib_mad"
+	ssh root@$remote_node "rmmod ib_uverbs"
+	ssh root@$remote_node "modprobe ib_wfr_lite"
+	ssh root@$remote_node "modprobe ib_umad"
+	ssh root@$remote_node "modprobe ib_uverbs"
+	ssh root@$remote_node "modprobe ib_usa"
+	sleep 5
+else
+	echo "Disabling remote VL15 over VL0 mode"
+	ssh root@${remote_node} "echo 0 > /sys/module/ib_wfr_lite/parameters/vl15_ovl0"
+fi
 ssh root@$remote_node "iba_portenable -w2 -s4 -p $remote_port"
 
-echo "Reloading ib_wfr_lite on \"localhost\""
-rmmod ib_qib
-rmmod ib_wfr_lite
-rmmod ib_umad
-rmmod ib_usa
-rmmod ib_sa
-rmmod ib_mad
-rmmod ib_uverbs
-modprobe ib_wfr_lite
-modprobe ib_umad
-modprobe ib_uverbs
-modprobe ib_usa
+if [ "$reload_drivers" == "true" ]; then
+	echo "Reloading ib_wfr_lite on \"localhost\""
+	rmmod ib_qib
+	rmmod ib_wfr_lite
+	rmmod ib_umad
+	rmmod ib_usa
+	rmmod ib_sa
+	rmmod ib_mad
+	rmmod ib_uverbs
+	modprobe ib_wfr_lite
+	modprobe ib_umad
+	modprobe ib_uverbs
+	modprobe ib_usa
+else
+	echo "Disabling local VL15 over VL0 mode"
+	echo 0 > /sys/module/ib_wfr_lite/parameters/vl15_ovl0
+fi
 
 iba_portenable -w2 -s4 -p $local_port
 
