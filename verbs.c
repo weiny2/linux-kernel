4d876615097a2de2f5bd7237ae081c58eaaa510b
Mike Marciniszyn <mike.marciniszyn@intel.com>
2013-11-04 12:34:16 -0500

    IB/hfi: correct header copy arg
    The code was mistakenly passing &hdr instead of hdr.
    
    Signed-off-by: Mike Marciniszyn <mike.marciniszyn@intel.com>

--- verbs.c
+++ verbs.c
@@ -1126,7 +1126,7 @@ static int qib_verbs_send_pio(struct qib_qp *qp, struct qib_ib_header *ibhdr,
 	if (len == 0) {
 		pio_copy(pbuf, pbc, hdr, hdrwords);
 	} else {
-		seg_pio_copy_start(pbuf, pbc, &hdr, hdrwords);
+		seg_pio_copy_start(pbuf, pbc, hdr, hdrwords);
 		while (len) {
 			void *addr = ss->sge.vaddr;
 			u32 slen = ss->sge.length;
