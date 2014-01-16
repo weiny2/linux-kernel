6a32a8e3f12f996bf78e934a8c268190688b59af
Dean Luick <dean.luick@intel.com>
2014-01-16 11:00:31 -0800

    Work with WFR HAS 0.75 (WFR simulator v31)
    Adjust the soft link to point to the new headers.
    
    wfr_core_defs.h replaces wfr_addrmap.h and dc_8051_csrs_defs.h.

--- wfr.h
+++ wfr.h
@@ -62,13 +62,12 @@
 #define USE_GENERATED_WFR_HEADERS 1
 #ifdef USE_GENERATED_WFR_HEADERS
 
-#include "include/wfr/wfr_addrmap.h"
+#include "include/wfr/wfr_core_defs.h"
 #include "include/wfr/wfr_cce_defs.h"
 #include "include/wfr/wfr_rxe_defs.h"
 #include "include/wfr/wfr_txe_defs.h"
 #include "include/wfr/wfr_misc_defs.h"
 #include "include/wfr/wfr_asic_defs.h"
-#include "include/wfr/dc_top_csrs_defs.h"
 #include "include/wfr/dc_8051_csrs_defs.h"
 #include "include/wfr/dcc_csrs_defs.h"
 #include "include/wfr/dc_lcb_csrs_defs.h"
