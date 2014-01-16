9178da8d5b0d713dada7cecba2eff46ef4664055
Hannes Reinecke <hare@suse.de>
2014-01-16 11:55:47 +0100

    - Review megaraid_sas patches
    - Refresh patches.drivers/megaraids-sas-boot-hangs-while-LD-is-offline.patch
    - Enable patches.drivers/megaraid_Disable-controller-reset-for-ppc.patch
    - Delete patches.drivers/megaraid_sas_fix_kdump.patch.
    
    suse-commit: 0ad23a15e8d4fb8702de01cc42d58f84c6094f36

--- drivers/scsi/megaraid/megaraid_sas_base.c
+++ drivers/scsi/megaraid/megaraid_sas_base.c
@@ -516,6 +516,17 @@ megasas_check_reset_ppc(struct megasas_instance *instance,
 	return 0;
 }
 
+/**
+ * megasas_adp_reset_ppc -  For controller reset
+ * @regs:                              MFI register set
+ */
+static int
+megasas_adp_reset_ppc(struct megasas_instance *instance,
+	struct megasas_register_set __iomem *regs)
+{
+	return 0;
+}
+
 static struct megasas_instance_template megasas_instance_template_ppc = {
 
 	.fire_cmd = megasas_fire_cmd_ppc,
@@ -523,7 +534,7 @@ static struct megasas_instance_template megasas_instance_template_ppc = {
 	.disable_intr = megasas_disable_intr_ppc,
 	.clear_intr = megasas_clear_intr_ppc,
 	.read_fw_status_reg = megasas_read_fw_status_reg_ppc,
-	.adp_reset = megasas_adp_reset_xscale,
+	.adp_reset = megasas_adp_reset_ppc,
 	.check_reset = megasas_check_reset_ppc,
 	.service_isr = megasas_isr,
 	.tasklet = megasas_complete_cmd_dpc,
@@ -1591,17 +1602,24 @@ static int megasas_slave_configure(struct scsi_device *sdev)
 	*        That will be fixed once LSI engineers have audited the
 	*        firmware for possible issues.
 	*/
-	if (sdev->channel < MEGASAS_MAX_PD_CHANNELS &&
-				sdev->type == TYPE_DISK) {
+	/*
+	 * Skip for LD offline and PD which are not configured as
+	 * system pd.
+	 */
+	if (sdev->channel < MEGASAS_MAX_PD_CHANNELS) {
+		if (sdev->type == TYPE_DISK)
+			return -ENXIO;
+
 		pd_index = (sdev->channel * MEGASAS_MAX_DEV_PER_CHANNEL) +
 								sdev->id;
-		if (instance->pd_list[pd_index].driveState ==
-						MR_PD_STATE_SYSTEM) {
-			blk_queue_rq_timeout(sdev->request_queue,
-				MEGASAS_DEFAULT_CMD_TIMEOUT * HZ);
-			return 0;
-		}
-		return -ENXIO;
+		if (instance->pd_list[pd_index].driveState !=
+						MR_PD_STATE_SYSTEM)
+			return -ENXIO;
+	} else {
+		ld_index = ((sdev->channel - MEGASAS_MAX_PD_CHANNELS) *
+			    MEGASAS_MAX_DEV_PER_CHANNEL) + sdev->id;
+		if (instance->ld_ids[ld_index] == 0xFF)
+			return -ENXIO;
 	}
 
 	/*
