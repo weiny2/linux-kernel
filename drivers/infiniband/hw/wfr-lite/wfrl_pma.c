ad28ed5caf950ef0cee03b9d7c934dff5cbadd27
Ira Weiny <ira.weiny@intel.com>
2014-01-23 18:44:06 -0500

    wfr-lite: update copyright on wfrl_pma.c

--- drivers/infiniband/hw/wfr-lite/wfrl_pma.c
+++ drivers/infiniband/hw/wfr-lite/wfrl_pma.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Intel Corporation.  All rights reserved.
+ * Copyright (c) 2012 - 2014 Intel Corporation.  All rights reserved.
  * Copyright (c) 2006 - 2012 QLogic Corporation. All rights reserved.
  * Copyright (c) 2005, 2006 PathScale, Inc. All rights reserved.
  *
