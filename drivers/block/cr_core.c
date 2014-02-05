516aa4dbaed25ff3b2f45446a2c664ebd139f196
Rob Gittins <robg.gittins@linux.intel.com>
2014-02-05 16:32:32 -0700

    added licenses and minor cleanup

--- drivers/block/cr_core.c
+++ drivers/block/cr_core.c
@@ -1,17 +1,31 @@
 /*
- * Copyright (c) 2013 Intel Corporation
+ * @internal
+ * @copyright
+ * Copyright 2013 Intel Corporation All Rights Reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 2 as published by the Free Software Foundation.
+ * INTEL CONFIDENTIAL
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * The source code contained or described herein and all documents related to
+ * the source code ("Material") are owned by Intel Corporation or its suppliers
+ * or licensors. Title to the Material remains with Intel Corporation or its
+ * suppliers and licensors. The Material may contain trade secrets and
+ * proprietary and confidential information of Intel Corporation and its
+ * suppliers and licensors, and is protected by worldwide copyright and trade
+ * secret laws and treaty provisions. No part of the Material may be used,
+ * copied, reproduced, modified, published, uploaded, posted, transmitted,
+ * distributed, or disclosed in any way without Intel's prior express written
+ * permission.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>
+ * No license under any patent, copyright, trade secret or other intellectual
+ * property right is granted to or conferred upon you by disclosure or delivery
+ * of the Materials, either expressly, by implication, inducement, estoppel or
+ * otherwise. Any license under such intellectual property rights must be
+ * express and approved by Intel in writing.
+ *
+ * Unless otherwise agreed by Intel in writing, you may not remove or alter
+ * this notice or any other notice embedded in Materials by Intel or Intel's
+ * suppliers or licensors in any way.
+ * @endinternal
  */
 
 #include <linux/crbd_dimm.h>
--- include/linux/nvdimm_core.h
+++ include/linux/nvdimm_core.h
@@ -1,3 +1,35 @@
+
+/*
+ * @internal
+ * @copyright
+ * Copyright 2013 Intel Corporation All Rights Reserved.
+ *
+ * INTEL CONFIDENTIAL
+ *
+ * The source code contained or described herein and all documents related to
+ * the source code ("Material") are owned by Intel Corporation or its suppliers
+ * or licensors. Title to the Material remains with Intel Corporation or its
+ * suppliers and licensors. The Material may contain trade secrets and
+ * proprietary and confidential information of Intel Corporation and its
+ * suppliers and licensors, and is protected by worldwide copyright and trade
+ * secret laws and treaty provisions. No part of the Material may be used,
+ * copied, reproduced, modified, published, uploaded, posted, transmitted,
+ * distributed, or disclosed in any way without Intel's prior express written
+ * permission.
+ *
+ * No license under any patent, copyright, trade secret or other intellectual
+ * property right is granted to or conferred upon you by disclosure or delivery
+ * of the Materials, either expressly, by implication, inducement, estoppel or
+ * otherwise. Any license under such intellectual property rights must be
+ * express and approved by Intel in writing.
+ *
+ * Unless otherwise agreed by Intel in writing, you may not remove or alter
+ * this notice or any other notice embedded in Materials by Intel or Intel's
+ * suppliers or licensors in any way.
+ * @endinternal
+ */
+
+
 #ifndef NVDIMM_CORE_H_
 #define NVDIMM_CORE_H_
 
@@ -189,7 +221,7 @@ struct extent_set {
 	__u32 interleave_size;
 	__u8 rsvd2[4];
 	__u16 physical_id[MAX_INTERLEAVE]; /*Physical IDs of the DIMMs that
-	 comprise this extent set*/
+		comprise this extent set*/
 };
 
 /*CR Specific?*/
@@ -232,7 +264,6 @@ struct nvm_volume {
 	struct mutex extent_set_lock;
 	struct list_head extent_set_list;
 };
-
 
 struct pmem_dev {
 	spinlock_t volume_lock;
