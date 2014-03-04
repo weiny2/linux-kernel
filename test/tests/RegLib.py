8c351ceb89df98304d861b02172478c68309a9da
Dennis Dalessandro <dennis.dalessandro@intel.com>
2014-03-04 14:22:10 -0800

    test/mpi: Append /host for non-standard PSM lib path on simics
    A previous check-in enabled MPI tests to use a custom PSM library, however the
    /host part was not gettign prepended to the path. This made LD_LIBRARY_PATH have
    an invalid path which is silently ignored and you end up running with the wrong
    PSM. This fix expose an is_simics query on the test object and fixes the path
    issue.
    
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>

--- test/tests/RegLib.py
+++ test/tests/RegLib.py
@@ -317,6 +317,9 @@ class TestInfo:
         else:
             return "DEFAULT"
 
+    def is_simics(self):
+        return self.simics
+
     def __str__(self):
         ret = ""
         for node in self.nodelist:
