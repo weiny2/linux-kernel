fae2adce99651ba8c6cea824080dc93b3df33320
Mike Galbraith <mgalbraith@suse.de>
2014-04-15 10:16:18 +0200

    sched: update_rq_clock() must skip ONE update (bnc#868528, bnc#869033).
    suse-commit: 16b4aaa1ce4f0b5acb9971bfc3314892aab01b4d

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -118,7 +118,7 @@ void update_rq_clock(struct rq *rq)
 {
 	s64 delta;
 
-	if (rq->skip_clock_update > 0)
+	if (rq->skip_clock_update-- > 0)
 		return;
 
 	delta = sched_clock_cpu(cpu_of(rq)) - rq->clock;
