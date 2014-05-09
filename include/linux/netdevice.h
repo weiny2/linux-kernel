bb457749114c99bf6a20be04d864bb95b3a85a45
Michal Kubecek <mkubecek@suse.cz>
2014-05-09 19:30:56 +0200

    net: Separate the close_list and the unreg_list v2 (fate#316924 bnc#875631 bnc#876145).
    suse-commit: cd8e6b0a8843699d9113003dddd45a4b0e8dff21

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1173,6 +1173,7 @@ struct net_device {
 	struct list_head	dev_list;
 	struct list_head	napi_list;
 	struct list_head	unreg_list;
+	struct list_head	close_list;
 
 	/* directly linked devices, like slaves for bonding */
 	struct {
