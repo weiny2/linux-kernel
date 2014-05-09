255a9275323ee819a8675de82ff6d17637928306
Michal Kubecek <mkubecek@suse.cz>
2014-05-09 19:30:56 +0200

    bonding: remove bond read lock for bond_3ad_state_machine_handler() (fate#316924 bnc#875631 bnc#876145).
    suse-commit: 9d1315e780af4ba746aa181a4598fb4332b892f4

--- drivers/net/bonding/bond_3ad.c
+++ drivers/net/bonding/bond_3ad.c
@@ -2066,8 +2066,10 @@ void bond_3ad_state_machine_handler(struct work_struct *work)
 	struct slave *slave;
 	struct port *port;
 
-	read_lock(&bond->lock);
-
+	if (!rtnl_trylock()) {
+		queue_delayed_work(bond->wq, &bond->ad_work, ad_delta_in_ticks);
+		return;
+	}
 	//check if there are any slaves
 	if (!bond_has_slaves(bond))
 		goto re_arm;
@@ -2120,9 +2122,8 @@ void bond_3ad_state_machine_handler(struct work_struct *work)
 	}
 
 re_arm:
+	rtnl_unlock();
 	queue_delayed_work(bond->wq, &bond->ad_work, ad_delta_in_ticks);
-
-	read_unlock(&bond->lock);
 }
 
 /**
