6c8cdb803a682be53c2cd9a07ef7a2f77e1c1a99
Jan Kara <jack@suse.cz>
2014-05-22 19:23:36 +0200

    xfs: remove newlines from strings passed to __xfs_printk.
    suse-commit: 7304429589964f19fae9abbc814a3d3ab172b483

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1287,7 +1287,7 @@ xfs_fs_remount(
 			 */
 #if 0
 			xfs_info(mp,
-		"mount option \"%s\" not supported for remount\n", p);
+		"mount option \"%s\" not supported for remount", p);
 			return -EINVAL;
 #else
 			break;
