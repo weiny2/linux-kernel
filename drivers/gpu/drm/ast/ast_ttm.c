437a936f5d02da65a83def5ea46d0c5eccc77476
Egbert Eich <eich@suse.de>
2014-06-11 20:10:15 +0200

    drm/ast: Simplify function ast_bo_unpin().
    suse-commit: 747258019d39b48886b22f1391313b6a7c1bbcdc

--- drivers/gpu/drm/ast/ast_ttm.c
+++ drivers/gpu/drm/ast/ast_ttm.c
@@ -373,7 +373,7 @@ int ast_bo_pin(struct ast_bo *bo, u32 pl_flag, u64 *gpu_addr)
 
 int ast_bo_unpin(struct ast_bo *bo)
 {
-	int i, ret;
+	int i;
 	if (!bo->pin_count) {
 		DRM_ERROR("unpin bad %p\n", bo);
 		return 0;
@@ -384,11 +384,8 @@ int ast_bo_unpin(struct ast_bo *bo)
 
 	for (i = 0; i < bo->placement.num_placement ; i++)
 		bo->placements[i] &= ~TTM_PL_FLAG_NO_EVICT;
-	ret = ttm_bo_validate(&bo->bo, &bo->placement, false, false);
-	if (ret)
-		return ret;
 
-	return 0;
+	return ttm_bo_validate(&bo->bo, &bo->placement, false, false);
 }
 
 int ast_bo_push_sysram(struct ast_bo *bo)
