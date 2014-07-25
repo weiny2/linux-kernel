bfa4c9fc494e88f4910b44cf26560ce6bf60637b
Hannes Reinecke <hare@suse.de>
2014-07-25 12:21:33 +0200

    qla2xxx: Update the driver version to 8.07.00.08.12.0-k (bnc#879633).
    suse-commit: 16caa0549dbe3b0431e3917dd85ffa5b36a12f19

--- drivers/scsi/qla2xxx/qla_version.h
+++ drivers/scsi/qla2xxx/qla_version.h
@@ -7,9 +7,9 @@
 /*
  * Driver version
  */
-#define QLA2XXX_VERSION      "8.06.00.14.12.0-k"
+#define QLA2XXX_VERSION      "8.07.00.08.12.0-k"
 
 #define QLA_DRIVER_MAJOR_VER	8
-#define QLA_DRIVER_MINOR_VER	6
+#define QLA_DRIVER_MINOR_VER	7
 #define QLA_DRIVER_PATCH_VER	0
 #define QLA_DRIVER_BETA_VER	0
