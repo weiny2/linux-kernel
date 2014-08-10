d9c3118287d364d3005b03bd0d3896c35105854f
Dan Williams <dan.j.williams@intel.com>
2014-08-10 11:31:08 -0400

    nd: dimm devices (nfit "memory-devices")
    Register the dimms described in the nfit as devices on a nd_bus, named
    "dimmN" where N is a global ida index.  The dimm numbering per-bus may
    appear contiguous, since we only allow a single nd_bus to be registered
    at at a time.  However, eventually, dimm-hotplug invalidates this
    property and dimms should be addressed via NFIT-handle.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/block/nd/nd-private.h
+++ drivers/block/nd/nd-private.h
@@ -15,6 +15,7 @@
 #include <linux/device.h>
 extern struct list_head nd_bus_list;
 extern struct mutex nd_bus_list_mutex;
+extern struct bus_type nd_bus_type;
 
 struct nd_bus {
 	struct nfit_bus_descriptor *nfit_desc;
@@ -24,6 +25,13 @@ struct nd_bus {
 	struct list_head dcrs;
 	struct list_head bdws;
 	struct list_head list;
+	struct device dev;
+	int id;
+};
+
+struct nd_dimm {
+	struct nfit_mem __iomem *nfit_mem;
+	struct nfit_dcr __iomem *nfit_dcr;
 	struct device dev;
 	int id;
 };
@@ -45,12 +53,15 @@ struct nd_bdw {
 
 struct nd_mem {
 	struct nfit_mem __iomem *nfit_mem;
+	struct nfit_dcr __iomem *nfit_dcr;
 	struct list_head list;
 };
 
 struct nd_bus *to_nd_bus(struct device *dev);
+struct nd_dimm *to_nd_dimm(struct device *dev);
 int __init nd_bus_init(void);
 void __exit nd_bus_exit(void);
 int nd_bus_create_ndctl(struct nd_bus *nd_bus);
 void nd_bus_destroy_ndctl(struct nd_bus *nd_bus);
+int nd_bus_register_dimms(struct nd_bus *nd_bus);
 #endif /* __ND_PRIVATE_H__ */
