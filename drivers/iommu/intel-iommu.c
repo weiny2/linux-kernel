9e0ea0f2bb5fdcc60dccf765a1873ab00f016070
Joerg Roedel <jroedel@suse.de>
2014-10-02 16:50:54 +0200

    iommu/vt-d: Only remove domain when device is removed (bnc#883139).
    suse-commit: 693284101e6e562e2fe3ed0b38c13928b6f52bda

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -3846,16 +3846,7 @@ static int device_notifier(struct notifier_block *nb,
 	if (iommu_dummy(dev))
 		return 0;
 
-	if (action != BUS_NOTIFY_UNBOUND_DRIVER &&
-	    action != BUS_NOTIFY_DEL_DEVICE)
-		return 0;
-
-	/*
-	 * If the device is still attached to a device driver we can't
-	 * tear down the domain yet as DMA mappings may still be in use.
-	 * Wait for the BUS_NOTIFY_UNBOUND_DRIVER event to do that.
-	 */
-	if (action == BUS_NOTIFY_DEL_DEVICE && dev->driver != NULL)
+	if (action != BUS_NOTIFY_REMOVED_DEVICE)
 		return 0;
 
 	domain = find_domain(dev);
