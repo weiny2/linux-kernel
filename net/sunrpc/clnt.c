513c14222ad5f7dd8cefa4525a2ee1659dcb15be
NeilBrown <neilb@suse.de>
2014-10-29 14:48:15 +1100

    SUNRPC: Handle EPIPE in xprt_connect_status (bnc#901090).
    suse-commit: c0effd9484f8abe79d5e6bc027263543a8876b20

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -1736,6 +1736,7 @@ call_connect_status(struct rpc_task *task)
 	case -ENETUNREACH:
 	case -EHOSTUNREACH:
 	case -ENOBUFS:
+	case -EPIPE:
 		if (RPC_IS_SOFTCONN(task))
 			break;
 		/* retry with existing socket, after a delay */
