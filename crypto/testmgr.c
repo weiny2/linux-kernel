74bef06640838946da041f95b52285fa00283e8c
Torsten Duwe <duwe@suse.de>
2014-11-13 17:10:28 +0100

    We need to activate cmac(aes) in FIPS mode (bnc#905296).
    suse-commit: 5be349643b1d026f210511501da1c9417bec0486

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -2117,6 +2117,7 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "cmac(aes)",
 		.test = alg_test_hash,
+		.fips_allowed = 1,
 		.suite = {
 			.hash = {
 				.vecs = aes_cmac128_tv_template,
