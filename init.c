71f2a77269d91bfded499d8cc2bc631ab4873243
Brian Welty <brian.welty@intel.com>
2014-11-17 17:13:51 -0800

    Initial attempt at modular FXR driver using intermediate bus.
    Signed-off-by: Brian Welty <brian.welty@intel.com>

--- init.c
+++ init.c
@@ -36,38 +36,11 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 #include "hfi.h"
-#include "user/device.h"
 #include "debugfs.h"
 
 /* TODO - multiple HFI support... */
 #define HFI_MAX_DEVICES 1
 static struct hfi_devdata *hfi_dev_tbl[HFI_MAX_DEVICES] = {0};
-
-/* TODO: module param? */
-static int create_ui = 1;
-
-/*
- * Define the driver version number.  This is something that refers only
- * to the driver itself, not the software interfaces it supports.
- */
-#ifndef HFI_DRIVER_VERSION_BASE
-#define HFI_DRIVER_VERSION_BASE "0.0"
-#endif
-
-/* create the final driver version string */
-#ifdef HFI_IDSTR
-#define HFI_DRIVER_VERSION HFI_DRIVER_VERSION_BASE " " HFI_IDSTR
-#else
-#define HFI_DRIVER_VERSION HFI_DRIVER_VERSION_BASE
-#endif
-
-MODULE_LICENSE("Dual BSD/GPL");
-MODULE_AUTHOR("Intel <ibsupport@intel.com>");
-MODULE_DESCRIPTION("Intel(R) STL Gen2 HFI Driver");
-MODULE_VERSION(HFI_DRIVER_VERSION);
-
-#define PCI_VENDOR_ID_INTEL 0x8086
-#define PCI_DEVICE_ID_INTEL_FXR0 0x26d0
 
 static struct pci_device_id hfi_pci_tbl[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_FXR0) },
@@ -112,71 +85,6 @@ struct hfi_devdata *hfi_alloc_devdata(struct pci_dev *pdev)
 }
 
 /*
- * Create per HFI device files in /dev
- */
-static int hfi_device_create(struct hfi_devdata *dd)
-{
-	int ret;
-
-	ret = hfi_user_add(dd);
-	if (!ret && create_ui)
-		ret = hfi_ui_add(dd);
-#ifdef HFI_DIAG_SUPPORT
-	if (!ret)
-		ret = hfi_diag_add(dd);
-#endif
-	return ret;
-}
-
-/*
- * Remove per-unit files in /dev
- */
-static void hfi_device_remove(struct hfi_devdata *dd)
-{
-	hfi_user_remove(dd);
-	hfi_ui_remove(dd);
-#ifdef HFI_DIAG_SUPPORT
-	hfi_diag_remove(dd);
-#endif
-}
-
-/*
- * Device initialization, called from PCI probe.
- */
-static int hfi_device_init(struct hfi_devdata *dd)
-{
-	int ret;
-
-	ret = hfi_device_create(dd);
-	if (ret)
-		dd_dev_err(dd, "Failed to create /dev devices: %d\n", -ret);
-
-	hfi_dbg_init(dd);
-	return 0;
-}
-
-/*
- * Perform required device shutdown logic, also remove /dev entries.
- * Called when unloading the driver.
- */
-static void hfi_device_shutdown(struct hfi_devdata *dd)
-{
-	hfi_dbg_exit();
-	hfi_device_remove(dd);
-}
-
-/*
- * Cleanup and free data structures. Performed after device shutdown.
- */
-static void hfi_device_cleanup(struct hfi_devdata *dd)
-{
-	struct pci_dev *pdev = dd->pcidev;
-
-	hfi_pci_dd_free(dd);
-	hfi_pci_cleanup(pdev);
-}
-
-/*
  * Device probe - where life begins.
  */
 static int hfi_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
@@ -208,17 +116,10 @@ static int hfi_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	if (ret)
 		goto err_dd_init;
 
-	/* Complete remaining device setup so HFI is ready for use. */
-	ret = hfi_device_init(dd);
-	if (ret)
-		goto err_device_init;
-
-	/* TODO - optionally register with IB core */
-	//register_ib_device(dd);
+	hfi_dbg_init(dd);
 
 	return 0;
 
-err_device_init:
 	hfi_pci_dd_free(dd);
 err_dd_init:
 	hfi_pci_cleanup(pdev);
@@ -230,9 +131,31 @@ static void hfi_pci_remove(struct pci_dev *pdev)
 {
 	struct hfi_devdata *dd = pci_get_drvdata(pdev);
 
-	/* TODO - unregister from IB core */
-	//unregister_ib_device(dd);
-
-	hfi_device_shutdown(dd);
-	hfi_device_cleanup(dd);
+	hfi_dbg_exit();
+	hfi_pci_dd_free(dd);
+	hfi_pci_cleanup(pdev);
 }
+
+int __init hfi_init(void)
+{
+	int ret;
+
+	ret = pci_register_driver(&hfi_driver);
+	if (ret < 0) {
+		pr_err("Unable to register driver: error %d\n", -ret);
+	}
+
+	return ret;
+}
+module_init(hfi_init);
+
+void hfi_cleanup(void)
+{
+	pci_unregister_driver(&hfi_driver);
+}
+module_exit(hfi_cleanup);
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Intel Corporation");
+MODULE_DESCRIPTION("Intel(R) STL Gen2 HFI Driver");
+MODULE_VERSION(HFI_DRIVER_VERSION);
--- makefile
+++ makefile
@@ -101,6 +101,8 @@ dist: distclean specfile headers
 install:
 	mkdir -p $(RPM_BUILD_ROOT)/lib/modules/$(KVER)/updates
 	install $(NAME).ko $(RPM_BUILD_ROOT)/lib/modules/$(KVER)/updates
+	install $(NAME)_bus.ko $(RPM_BUILD_ROOT)/lib/modules/$(KVER)/updates
+	install $(NAME)_user.ko $(RPM_BUILD_ROOT)/lib/modules/$(KVER)/updates
 
 rpm: dist
 	rpmbuild --define 'require_kver $(KVER)' -ta $(NAME)-$(VERSION).tgz
