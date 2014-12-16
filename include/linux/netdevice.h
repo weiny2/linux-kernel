f92ba80cf24068d109ad61b0cf195730f746c2af
Michal Kubecek <mkubecek@suse.cz>
2014-12-16 13:22:03 +0100

    net: Find the nesting level of a given device by type (bnc#829110 bnc#891277 bnc#904053).
    suse-commit: c9ace14e38e31f22bea7553fe63d1bdff5944b75

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2899,6 +2899,14 @@ extern void *netdev_lower_get_next_private_rcu(struct net_device *dev,
 	     priv; \
 	     priv = netdev_lower_get_next_private_rcu(dev, &(iter)))
 
+void *netdev_lower_get_next(struct net_device *dev,
+			    struct list_head **iter);
+#define netdev_for_each_lower_dev(dev, ldev, iter) \
+	for (iter = &(dev)->adj_list.lower, \
+	     ldev = netdev_lower_get_next(dev, &(iter)); \
+	     ldev; \
+	     ldev = netdev_lower_get_next(dev, &(iter)))
+
 extern void *netdev_adjacent_get_private(struct list_head *adj_list);
 void *netdev_lower_get_first_private_rcu(struct net_device *dev);
 extern struct net_device *netdev_master_upper_dev_get(struct net_device *dev);
@@ -2914,6 +2922,8 @@ extern void netdev_upper_dev_unlink(struct net_device *dev,
 				    struct net_device *upper_dev);
 extern void *netdev_lower_dev_get_private(struct net_device *dev,
 					  struct net_device *lower_dev);
+int dev_get_nest_level(struct net_device *dev,
+		       bool (*type_check)(struct net_device *dev));
 extern int skb_checksum_help(struct sk_buff *skb);
 extern struct sk_buff *__skb_gso_segment(struct sk_buff *skb,
 	netdev_features_t features, bool tx_path);
