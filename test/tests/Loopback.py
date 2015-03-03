weiny2/linux-kernel

commit 64c234a91e7459394b1545c525d7a7c66b8a995f

IB/hfi: Only go to Polling if cable module is present
When the cable is pulled out of the port, WFR transitions
to Offline state and then moves to Polling state instead
of remaining in Offline state. This patch checks for the
presence of the module before re-enabling the link and
if not present, prevents link bringup, and schedules a
link restart. This patch has been resubmitted as the VC2k
FPGA failure has been resolved.

INTERNAL: PR 124909 - HCA goes to Polling instead of Offline
INTERNAL: when cable is disconnected
INTERNAL: PR 126357 - Using IFS 10.0.0.0.274 VC boards cannot
INTERNAL: detect link
INTERNAL: HSD 291604 - VC709 board's FPGA release does not
INTERNAL: manage QSFP MODPRST_N mapping to SFP 1-4 MOD_DETECT

Reviewed-by: Dean Luick <dean.luick@intel.com>
Signed-off-by: Easwar Hariharan <easwar.hariharan@intel.com>
Change-Id: Idd1c1168e042216ba525c4b230aea2173b95b94e

wfr.c
@@ -5131,11 +5131,21 @@ static void restart_link(struct qib_pportdata *ppd)
 {
 	int ret;
 
-	/* don't try to restart if the link is not enabled */
+	/*
+	 * don't try to restart if the link is not enabled or
+	 * if there is no cable present
+	 */
 	if (!ppd->link_enabled) {
 		dd_dev_info(ppd->dd,
 			"%s: stopping link restart because link is disabled\n",
 			__func__);
+		return;
+	}
+	if (!qsfp_mod_present(ppd)) {
+		dd_dev_info(ppd->dd,
+			"%s: stopping link restart because cable is missing\n",
+			__func__);
+		schedule_link_restart(ppd);
 		return;
 	}
 	ret = set_link_state(ppd, HLS_DN_POLL);
@@ -5189,9 +5199,16 @@ static int bringup_serdes(struct qib_pportdata *ppd)
 		ret = init_loopback(dd);
 		if (ret < 0)
 			return ret;
-		/* otherwise, proceed to polling */
 	}
-	return set_link_state(ppd, HLS_DN_POLL);
+	if (qsfp_mod_present(ppd))
+		return set_link_state(ppd, HLS_DN_POLL);
+
+	dd_dev_err(ppd->dd, "Cable not present, attempting link restart in 10 seconds\n");
+	/* Placing schedule_link_restart here assumes that restart_link(..) sets
+	 * link state to HLS_DN_POLL
+	 */
+	schedule_link_restart(ppd);
+	return -EINVAL;
 }
 
 static void quiet_serdes(struct qib_pportdata *ppd)
@@ -5648,7 +5665,6 @@ static int goto_offline(struct qib_pportdata *ppd, u8 rem_reason)
 	ppd->link_width_active = 0;
 	ppd->link_width_downgrade_tx_active = 0;
 	ppd->link_width_downgrade_rx_active = 0;
-
 	schedule_link_restart(ppd);
 	return 0;
 }

qsfp.c
@@ -289,7 +289,7 @@ int qib_refresh_qsfp_cache(struct qib_pportdata *ppd, struct qib_qsfp_cache *cp)
 	/* ensure sane contents on invalid reads, for cable swaps */
 	memset(cp, 0, sizeof(*cp));
 
-	if (!qib_qsfp_mod_present(ppd)) {
+	if (!qsfp_mod_present(ppd)) {
 		ret = -ENODEV;
 		goto bail;
 	}
@@ -438,12 +438,18 @@ const char * const qib_qsfp_devtech[16] = {
 
 static const char *pwr_codes = "1.5W2.0W2.5W3.5W";
 
-int qib_qsfp_mod_present(struct qib_pportdata *ppd)
+int qsfp_mod_present(struct qib_pportdata *ppd)
 {
-	int ret;
+	if (HFI_CAP_IS_KSET(QSFP_ENABLED)) {
+		struct hfi_devdata *dd = ppd->dd;
+		u64 reg;
 
-	ret = ppd->dd->f_gpio_mod(ppd->dd, ppd->dd->hfi_id, 0, 0, 0);
-	return !(ret & QSFP_HFI0_MODPRST_N);
+		reg = read_csr(dd,
+			dd->hfi_id ? WFR_ASIC_QSFP2_IN : WFR_ASIC_QSFP1_IN);
+		return !(reg & QSFP_HFI0_MODPRST_N);
+	}
+	/* always return cable present */
+	return 1;
 }
 
 /*

qsfp.h
@@ -184,7 +184,7 @@ struct qib_qsfp_data {
 
 extern int qib_refresh_qsfp_cache(struct qib_pportdata *ppd,
 				  struct qib_qsfp_cache *cp);
-extern int qib_qsfp_mod_present(struct qib_pportdata *ppd);
+extern int qsfp_mod_present(struct qib_pportdata *ppd);
 extern void qib_qsfp_init(struct qib_pportdata *ppd);
 
 extern int i2c_write(struct qib_pportdata *ppd, u32 target, int i2c_addr,

commit 8b485a74e124f6668af582aeef54dc053ca41657

IB/hfi: Fix link start for loopback
Commit 64c234a91e7459394b1545c525d7a7c66b8a995f
broke link handling in any of the internal
loopback modes (LCB and Serdes) resulting in
the link never coming out of OFFLINE.

This commit fixes that issue and adds a loopback
test to the test harness (part of the default and
quick sets).

INTERNAL: PR127274 - Commit 64c234a91e7459394b1545c525d7a7c66b8a995f
INTERNAL: breaks loopback mode
INTERNAL: PR127285 - [harness] Add loopback (LCB and
INTERNAL: Serdes) test

Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
Reviewed-by: Dean Luick <dean.luick@intel.com>
Signed-off-by: Mitko Haralanov <mitko.haralanov@intel.com>
Change-Id: I7da20c313bbab518a50417747eb225b18521b6b3

wfr.c
@@ -5150,7 +5150,8 @@ static int start_link(struct qib_pportdata *ppd)
 		return 0;
 	}
 
-	if (qsfp_mod_present(ppd))
+	if (qsfp_mod_present(ppd) ||
+	    loopback == LOOPBACK_SERDES || loopback == LOOPBACK_LCB)
 		ret = set_link_state(ppd, HLS_DN_POLL);
 
 	/* TODO: do not poll when cable plug notification is available */

test/harness.py
@@ -308,6 +308,12 @@ test_list = [
       "desc" : "Run the Intel MPI Test Suite"
     },
 
+    { "test_name" : "Loopback-Test",
+      "test_exe" : "Loopback.py",
+      "args" : "--nodelist %HOST[1]% --hfisrc %HFI_SRC% --psm %PSM_LIB% --psmopts %PSM_OPTS% --args \"-L 2 -M 2 -w 3 -m 1048576 -z\"",
+      "type" : "default,quick",
+      "desc" : "Test loopback. LCB on Simics, Serdes on FPGA, both on ASIC."
+    },
 ]
 
 

test/tests/LoadModule.py
@@ -97,15 +97,13 @@ def main():
     print test_info
 
     count = test_info.get_host_count()
-    if count < 2:
-        RegLib.test_fail("Not enough hosts")
 
     print "Found ", count, "hosts"
-    host1 = test_info.get_host_record(0)
-    print host1
-
-    host2 = test_info.get_host_record(1)
-    print host2
+    hostlist = []
+    for x in range(count):
+        host = test_info.get_host_record(x)
+        print host
+        hostlist.append(host)
 
     ################
     # body of test #
@@ -121,27 +119,26 @@ def main():
     sm = test_info.which_sm()
 
     matchObj = re.match(r"(.*):(.*)", driver_parms)
-    host1_parms = ""
-    host2_parms = ""
+    host_parms = ["", ""]
     if matchObj:
         RegLib.test_log(5, "Using new style modparms");
-        host1_parms = matchObj.group(1)
-        host2_parms = matchObj.group(2)
+        host_parms[0] = matchObj.group(1)
+        host_parms[1] = matchObj.group(2)
     else:
         RegLib.test_log(5, "Using old style modparms");
-        host1_parms = driver_parms
-        host2_parms = driver_parms
+        host_parms[0] = driver_parms
+        host_parms[1] = driver_parms
 
-    host1_parms = host1_parms.strip()
-    host2_parms = host2_parms.strip()
-    RegLib.test_log(5, "host1 parms: %s" %host1_parms)
-    RegLib.test_log(5, "host2 parms: %s" %host2_parms)
+
+    host_parms = [x.strip() for x in host_parms]
+    for x in range(len(host_parms)):
+        RegLib.test_log(5, "host%d parms: %s" % (x, host_parms[x]))
       
     opafm_host = None
     
     if sm != "none":
         RegLib.test_log(0, "Trying to determine which node the fm is running on")
-        for host in host1, host2:
+        for host in hostlist:
             active = is_sm_active(host, "opafm")
             if active == 0:
                 if opafm_host == None:
@@ -164,7 +161,7 @@ def main():
             #time.sleep(15)
 
     # Go ahead and get the driver loaded or reloaded on both hosts
-    for host in host1,host2:
+    for host in hostlist:
         name = host.get_name()
         loaded = is_driver_loaded(host, driver_name)
         if loaded == True:
@@ -177,10 +174,7 @@ def main():
         #time.sleep(10)
 
         RegLib.test_log(0, name + " Loading Driver")
-        if host == host1:
-            driver_parms = host1_parms
-        else:
-            driver_parms = host2_parms
+        driver_parms = host_parms[hostlist.index(host)]
 
         loaded = load_driver(host, driver_name, driver_path, driver_parms)
         if loaded == True:
@@ -195,7 +189,7 @@ def main():
         RegLib.test_pass("Driver loaded sm/fm not running")
 
     if opafm_host == None: #chose host1 by default
-        opafm_host = host1
+        opafm_host = hostlist[0]
     RegLib.test_log(0, "Starting opafm on %s" % opafm_host.get_name())
     start_sm(opafm_host, "opafm")
     active = is_sm_active(opafm_host, "opafm")
@@ -205,7 +199,7 @@ def main():
     # Driver loaded and sm ready now wait till links are active on both
     # nodes.
     num_loaded = 0
-    for host in host1,host2:
+    for host in hostlist:
         name = host.get_name()
         err = wait_for_active(host, 10, 6)
         if err:
@@ -214,12 +208,12 @@ def main():
             RegLib.test_log(0, name + " Adapter is up and running")
             num_loaded = num_loaded + 1
 
-    if num_loaded != 2:
+    if num_loaded != len(hostlist):
         RegLib.test_fail(name + " Unable to get active state on at least 1 node")
 
     # Before bailing out dump the kmod load address in case we need to do some
     # debugging later.
-    for host in host1,host2:
+    for host in hostlist:
         print host.get_name(), "module load address is:"
         cmd = "cat /sys/module/hfi/sections/.init.text"
         (err, out) = do_ssh(host, cmd)

test/tests/Loopback.py
@@ -1,0 +1,128 @@
+#!/usr/bin/python
+#
+# Author: Mitko Haralanov <mitko.haralanov@intel.com>
+
+import RegLib
+import time
+import sys
+import os
+import subprocess
+import fcntl
+import shlex
+
+topdir = os.path.dirname(os.path.realpath(__file__))
+
+SUBTEST_LIST = ["MpiStress"]
+
+def run_subtest(cmd):
+    output = ""
+    args = shlex.split(cmd)
+    print "Going to run:", args
+    proc = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT, close_fds=True)
+    flags = fcntl.fcntl(proc.stdout.fileno(), fcntl.F_GETFL)
+    fcntl.fcntl(proc.stdout.fileno(), fcntl.F_SETFL, flags | os.O_NONBLOCK)
+    status = proc.poll()
+    while status == None:
+        try:
+            out = proc.stdout.read(128)
+            while out:
+                sys.stdout.write(out)
+                output += out
+                out = proc.stdout.read(128)
+        except IOError: pass
+        status = proc.poll()
+    out = proc.stdout.read(128)
+    while out:
+        sys.stdout.write(out)
+        output += out
+        out = proc.stdout.read(128)
+    return (status, output.split("\n"))
+
+def reload_module(hostlist, src=None, opts=[]):
+    optstr = "--nodelist=%s" % ",".join([x.get_name() for x in hostlist])
+    if src: optstr += " --hfisrc=%s" % src
+    if opts:
+        o = []
+        for x in range(len(hostlist)):
+            if opts[x]:
+                o.append(" ".join(["%s=%s" % (k, v) for k, v in opts[x].items()]))
+        if o: optstr += " --modparm=\"%s\"" % " : ".join(o)
+    cmd = os.path.join(topdir, "LoadModule.py") + " " + optstr
+    RegLib.test_log(0, "Executing %s..." % cmd)
+
+    st, out = run_subtest(cmd)
+    if st != 0: return False
+    return True
+
+def run_loopback_tests(test, hostlist, psm="DEFAULT", opts="", args=None):
+    optstr = ["--nodelist=%s" % ",".join([x.get_name() for x in hostlist])]
+    if psm != "DEFAULT": optstr.append("--psm=%s" % psm)
+    if opts: optstr.append("--psmopts=\"%s\"" % opts)
+    if args: optstr.append("--args=\"%s\"" % " ".join(args))
+    cmd = os.path.join(topdir, test+".py") + " ".join([""] + optstr)
+    RegLib.test_log(0, "Executing %s..." % cmd)
+    st, out = run_subtest(cmd)
+    if st != 0: return False
+    return True
+
+def main():
+    global topdir
+    clean = False
+
+    for test in SUBTEST_LIST + ["LoadModule"]:
+        if not os.path.exists(os.path.join(topdir, test+".py")):
+            RegLib.test_fail("Could not find dependency test script: %s" % test)
+
+    test_info = RegLib.TestInfo()
+    module_src = test_info.get_hfi_src()
+    host1 = test_info.get_host_record(0)
+
+    opts = test_info.get_mod_params_dict(host1.get_name())
+    # get PSM opts and strip the encosing quotes
+    psm_opts = test_info.get_psm_opts()[1:-1]
+    psm_opts += " -x PSM_DEVICES=self,hfi -x PSM_HFI_LOOPBACK=1"
+    psm_lib = test_info.get_psm_lib()
+
+    args = test_info.parse_extra_args()
+    if not args or (len(args) == 1 and not args[0]):
+        args = ["-L 1", "-M 1", "-w 2", "-z"]
+    args.append("-i")
+
+    if not test_info.is_fpga():
+        # LCB loopback
+        loopback_opts = {"loopback" : 2}
+        loopback_opts.update(opts)
+
+        if not reload_module([host1], module_src, [loopback_opts]):
+            RegLib.test_fail("Failed to restart driver with LCB loopback")
+        else:
+            clean = True
+
+        for test in SUBTEST_LIST:
+            if not run_loopback_tests(test, [host1], psm_lib, psm_opts, args):
+                RegLib.test_fail("Failed to execute subtest: %s" % test)
+
+    if not test_info.is_simics():
+        # SerDes loopback
+        loopback_opts = {"loopback" : 1}
+        loopback_opts.update(opts)
+
+        if not reload_module([host1], module_src, [loopback_opts]):
+            RegLib.test_fail("Failed to restart the driver with SerDes loopback")
+        else:
+            clean = True
+
+        for test in SUBTEST_LIST:
+            if not run_loopback_tests(test, [host1], psm_lib, psm_opts, args):
+                RegLib.test_fail("Failed to execute subtest: %s" % test)
+
+    if clean:
+        if not reload_module([host1], module_src, [opts]):
+            RegLib.test_fail("Failed to restore the driver")
+
+    RegLib.test_pass("Test with LCB and SerDes loopback passed.")
+    return
+
+if __name__ == "__main__":
+    main()

test/tests/RegLib.py
@@ -368,6 +368,7 @@ class TestInfo:
     hfi_src = None
     kbuild_dir = None
     simics = False
+    fpga = False
     mpiverbs = False
     paramdict = None
     np = "6"
@@ -377,6 +378,9 @@ class TestInfo:
 
     def is_simics(self):
         return self.simics
+
+    def is_fpga(self):
+        return self.fpga
 
     def get_parse_standard_args(self):
         parser = OptionParser()
@@ -407,6 +411,10 @@ class TestInfo:
         parser.add_option("--simics", action="store_true", dest="simics",
                           help="Run on simics environment. Optional if using "
                                + "viper0,viper1 as nodelist")
+
+        parser.add_option("--fpga", action="store_true", dest="fpga",
+                          help="Run on FPGA environment. Optional if using "
+                          + "fpga* as nodelist")
 
         parser.add_option("--type", dest="test_types",
                           help="Test type(s) to run. Use 'all' for everything."
@@ -470,6 +478,10 @@ class TestInfo:
         else:
             self.force_root = True
 
+        self.fpga = options.fpga
+        if self.fpga == None:
+            self.fpga = False
+
         if options.mpiverbs:
             self.mpiverbs = True
 
@@ -492,6 +504,10 @@ class TestInfo:
             for node in nodelist:
                 if node == "viper0" or node == "viper1":
                     self.simics = True
+                    break
+                elif "fpga" in node:
+                    self.fpga = True
+                    break
 
             for node in nodelist:
                 found_localhost = 0