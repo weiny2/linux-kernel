9262e3c83a43ccc3d400cd65e583b80f9afffa01
Sanath Kumar <sanath.s.kumar@intel.com>
2015-03-12 02:41:56 -0700

    Add wrappers to call fxr tests currently implemented
    Examples to launch these tests using harness:
    
    ./harness.py --nodelist=viper0 --type=default
    ./harness.py --nodelist=viper0 --type=quick
    
    The default option loads the module first before running the
    tests. The tests are assumed to be pre installed in the
    host that the harness is try to invoke from.
    
    Reviewed-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Signed-off-by: Sanath Kumar <sanath.s.kumar@intel.com>

---
+++ test/tests/HfiRing.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import RegLib
+import time
+import sys
+import os
+import subprocess
+import fcntl
+import shlex
+
+topdir = os.path.dirname(os.path.realpath(__file__))
+
+def do_ssh(host, cmd):
+    RegLib.test_log(5, "Running " + cmd)
+    return (host.send_ssh(cmd))
+
+def main():
+    global topdir
+    curr_file = os.path.basename(__file__)
+
+    test_info = RegLib.TestInfo()
+    host = test_info.get_host_record(0)
+
+    RegLib.test_log(0, "Test: " + curr_file + " started")
+    cmd = "hfi_ring"
+    (err, out) = do_ssh(host, cmd)
+    for line in out:
+        print RegLib.chomp(line)
+    if err:
+        RegLib.test_fail("hfi_ring failed")
+
+    RegLib.test_pass("hfi_ring passed")
+
+
+if __name__ == "__main__":
+    main()
