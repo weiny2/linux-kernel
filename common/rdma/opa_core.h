e03d58e909bf8ed6e17563e45adc121460f0cdec
Sudeep Dutt <sudeep.dutt@intel.com>
2015-03-27 09:34:17 -0700

    OPA core header file documentation cleanups
    Cc: Brian Welty <brian.welty@intel.com>
    Reviewed-by: Sanath Kumar <sanath.s.kumar@intel.com>
    Signed-off-by: Sudeep Dutt <sudeep.dutt@intel.com>

--- common/rdma/opa_core.h
+++ common/rdma/opa_core.h
@@ -49,41 +49,34 @@
  *
  * Intel(R) Omni-Path Core Driver Interface
  */
-
 #ifndef _OPA_CORE_H_
 #define _OPA_CORE_H_
-/*
- * Everything a opa_core driver needs to work with any particular opa_core
- * implementation.
- */
 #include <linux/device.h>
 #include <linux/idr.h>
 #include <rdma/hfi_types.h>
 
 /*
- * These macros are required for compiling against the
- * OPA headers shared with user space.
+ * TODO: These macros are required for compiling against the OPA headers
+ * shared with user space. Delete from this file eventually
  */
 #define _HFI_STATIC_INLINE static inline
-#define _HFI_PRINTF(FMT,args...)
+#define _HFI_PRINTF(FMT, args...)
 #define _HFI_ASSERT(X)
 #define HFI_TEST_ERR(x, y)
 #define assert(x)
-#define _align64 __attribute__((__aligned__(64)))
+#define _align64 __aligned(64)
 
 /*
- * HFI API data types
- * TODO - common provider integration
- * Based on hfi_ctx in common provider.
- * hfi_ctx needs to get moved/merged with data types defined by the common
- * provider logic.
+ * TODO - Based on hfi_ctx in common provider. hfi_ctx needs to get moved/merged
+ * with data types defined by the common provider logic.
  */
 struct hfi_devdata;
 
 /**
- * hfi_ctx - state for HFI resources assigned to this context.
- * @pid: Assigned Portals Process ID. The user space hfi_ctx has this field
- * @fd dummy file descriptor. The user space hfi_ctx has this field
+ * struct hfi_ctx - state for HFI resources assigned to this context
+ * @devdata: HFI device specific data, private to the hardware driver
+ * @pid: dummy Portals Process ID. The user space hfi_ctx has this field
+ * @fd: dummy file descriptor. The user space hfi_ctx has this field
  * @ptl_pid: Assigned Portals Process ID
  * @ptl_uid: Assigned Protection Domain ID
  * @ptl_state_base: Pointer to Portals state in host memory
@@ -105,16 +98,15 @@ struct hfi_devdata;
  * @cq_pair_num_assigned: Counter of assigned Command Queues
  * @eq_used: IDR table of allocated Events Queues
  * @eq_lock: Lock for EQ table
- * @ct_addr - CT size
- * @ct_size - pointer to the HFI Portal table (RO)
- * @pt_addr - PT size
- * @pt_size - PT size
- * @eq_addr - pointer to the HFI EQ desc table (RO)
- * @eq_size - EQ size
- * @eq_head_addr - pointer to the HFI EQ head pointers table (RW)
- * @eq_head_size - EQ head pointers size
+ * @ct_addr: Pointer to the counting events array
+ * @ct_size: Size of counting events array
+ * @pt_addr: Pointer to portal table entries
+ * @pt_size: Size of portal table entries
+ * @eq_addr: Pointer to the event queue desc table
+ * @eq_size: Size of event queue desc table
+ * @eq_head_addr: Pointer to the event queue head pointers table
+ * @eq_head_size: Size of the event queue head pointers table
  */
-/* TODO: remove Portals naming references if possible */
 struct hfi_ctx {
 	struct hfi_devdata *devdata;
 	int	fd;
@@ -150,9 +142,21 @@ struct hfi_ctx {
 	ssize_t	eq_head_size;
 };
 
-/*
- * Types of memory regions mapped into user processes' space
- * Used with ctx_addr operation.
+/**
+ * enum mmap_token_types -  Types of memory regions mapped by the context and
+ * @TOK_CONTROL_BLOCK: map process control block
+ * @TOK_PORTALS_TABLE: map portal table entries
+ * @TOK_CQ_TX: map TX command queue
+ * @TOK_CQ_RX: map RX command queue
+ * @TOK_CQ_HEAD: map command queue head
+ * @TOK_EVENTS_CT: map counting events array
+ * @TOK_EVENTS_EQ_DESC: map event queue desc array
+ * @TOK_EVENTS_EQ_HEAD: map event queue head
+ * @TOK_TRIG_OP: map triggered operations desc array
+ * @TOK_LE_ME: map array of LE/ME entries
+ * @TOK_UNEXPECTED: map array of unexpected headers
+ *
+ * Used with ctx_addr operation
  */
 enum mmap_token_types {
 	TOK_CONTROL_BLOCK = 1,
@@ -169,14 +173,13 @@ enum mmap_token_types {
 };
 
 /**
- * opa_ctx_assign - Used with opa_core_ops.ctx_assign operation.
+ * struct opa_ctx_assign - Used with opa_core_ops.ctx_assign operation
  * @pid: Requested Portals PID or -1 for driver assignment
  * @flags: Reserved for choosing alternate PID allocation behavior
  * @le_me_count: Requested size of LE/ME buffer
  * @unexpected_count: Requested size of unexpected headers buffer
  * @trig_op_count: Requested size of triggered ops buffer
- *
- * With @ctx_assign, assigned PID returned in ctx->ptl_pid.
+ * Used with @ctx_assign, assigned PID returned in ctx->ptl_pid
  */
 struct opa_ctx_assign {
 	u16 pid;
@@ -190,60 +193,53 @@ struct hfi_eq_assign_args;
 struct hfi_dlid_assign_args;
 
 /**
- * opa_core_ops - Hardware operations for accessing an OPA device on the OPA bus.
- * @ctx_assign: Assign a Send/Receive context of the HFI.
- * @ctx_release: Release Send/Receive context in the HFI.
- * @ctx_reserve: Reserve range of contiguous Send/Receive contexts in the HFI.
- * @ctx_unreserve: Release reservation from ctx_reserve.
- * @ctx_addr: Return address for HFI resources providing memory access/mapping.
- * @cq_assign: Assign a Command Queue for HFI send/receive operations.
- * @cq_update: Update configuration of Command Queue.
- * @cq_release: Release Command Queue.
- * @eq_assign: Assign an Event Completion Queue.
- * @eq_release: Release an Event Completion Queue.
- * @dlid_assign: Assign entries from the DLID relocation table.
- * @dlid_release: Release entries from the DLID relocation table.
+ * struct opa_core_ops - HW operations for accessing an OPA device
+ * @ctx_assign: Assign a Send/Receive context of the HFI
+ * @ctx_release: Release Send/Receive context in the HFI
+ * @ctx_reserve: Reserve range of contiguous Send/Receive contexts in the HFI
+ * @ctx_unreserve: Release reservation from ctx_reserve
+ * @ctx_addr: Return address for HFI resources providing memory access/mapping
+ * @cq_assign: Assign a Command Queue for HFI send/receive operations
+ * @cq_update: Update configuration of Command Queue
+ * @cq_release: Release Command Queue
+ * @eq_assign: Assign an Event Completion Queue
+ * @eq_release: Release an Event Completion Queue
+ * @dlid_assign: Assign entries from the DLID relocation table
+ * @dlid_release: Release entries from the DLID relocation table
  */
 struct opa_core_ops {
-	/* Resource Allocation ops */
-	int (*ctx_assign)(struct hfi_ctx *ctx, struct opa_ctx_assign *ctx_assign);
+	int (*ctx_assign)(struct hfi_ctx *ctx,
+			  struct opa_ctx_assign *ctx_assign);
 	void (*ctx_release)(struct hfi_ctx *ctx);
 	int (*ctx_reserve)(struct hfi_ctx *ctx, u16 *base, u16 count);
 	void (*ctx_unreserve)(struct hfi_ctx *ctx);
 	int (*ctx_addr)(struct hfi_ctx *ctx, int type, u16 ctxt, void **addr,
 			ssize_t *len);
-	int (*cq_assign)(struct hfi_ctx *ctx, struct hfi_auth_tuple *auth_table, u16 *cq_idx);
-	int (*cq_update)(struct hfi_ctx *ctx, u16 cq_idx, struct hfi_auth_tuple *auth_table);
+	int (*cq_assign)(struct hfi_ctx *ctx,
+			 struct hfi_auth_tuple *auth_table, u16 *cq_idx);
+	int (*cq_update)(struct hfi_ctx *ctx, u16 cq_idx,
+			 struct hfi_auth_tuple *auth_table);
 	int (*cq_release)(struct hfi_ctx *ctx, u16 cq_idx);
-	int (*eq_assign)(struct hfi_ctx *ctx, struct hfi_eq_assign_args *eq_assign);
+	int (*eq_assign)(struct hfi_ctx *ctx,
+			 struct hfi_eq_assign_args *eq_assign);
 	int (*eq_release)(struct hfi_ctx *ctx, u16 eq_idx);
 	int (*dlid_assign)(struct hfi_ctx *ctx,
 			   struct hfi_dlid_assign_args *dlid_assign);
 	int (*dlid_release)(struct hfi_ctx *ctx);
-
-#if 0
-	/* TODO - below is starting attempt at 'kernel provider' operations */
-	/* critical functions would be inlined instead of bus operation */
-	/* RX and completion ops */
-	int (*rx_alloc)(struct hfi_ctx *ctx, struct hfi_rx_alloc_args *rx_alloc);
-	int (*rx_free)(struct hfi_ctx *ctx, u16 rx_type, u32 rx_idx);
-	int (*rx_enable)(struct hfi_ctx *ctx, u16 rx_type, u32 rx_idx);
-	int (*rx_disable)(struct hfi_ctx *ctx, u16 rx_type, u32 rx_idx);
-	int (*eq_alloc)(struct hfi_ctx *ctx, struct hfi_eq_alloc_args *eq_alloc);
-	int (*eq_free)(struct hfi_ctx *ctx, u16 eq_type, u16 eq_idx);
-	int (*eq_get)(struct hfi_ctx *ctx, u16 eq_type, u16 eq_idx);
-	/* TX ops */
-	int (*tx_command)(struct hfi_ctx *ctx, u64 cmd, void *header, void *payload);
-#endif
 };
 
+/**
+ * struct opa_core_device - device and vendor ID for an opa core device
+ * @vendor: OPA HFI PCIe vendor ID
+ * @device: OPA HFI PCIe device ID
+ */
 struct opa_core_device_id {
 	__u32 vendor;
 	__u32 device;
 };
 
 /**
- * opa_core_device - representation of a device using opa_core
+ * struct opa_core_device - representation of a device using opa_core
  * @bus_ops: the hardware ops supported by this device.
  * @id: the device type identification (used to match it with a driver).
  * @dev: underlying device.
@@ -263,12 +259,32 @@ struct opa_core_device {
 };
 
 /**
- * opa_core_client - representation of a opa_core client
+ * opa_core_register_device - Register a hardware driver with the OPA core
+ * @dev: The hardware device
+ * @id: device and vendor ID
+ * @dd: device specific data
+ * @bus_ops: operations used by clients to access the hardware
+ *
+ * Return: an opa_core_device upon success or appropriate error
+ */
+struct opa_core_device *
+opa_core_register_device(struct device *dev, struct opa_core_device_id *id,
+			 struct hfi_devdata *dd, struct opa_core_ops *bus_ops);
+/**
+ * opa_core_unregister_device - unregister a hardware driver from the OPA core
+ * @odev: opa core device
+ *
+ * Return: none
+ */
+void opa_core_unregister_device(struct opa_core_device *odev);
+
+/**
+ * struct opa_core_client - representation of an opa_core client
  *
  * @name: OPA client name
  * @add: the function to call when a device is discovered
  * @remove: the function to call when a device is removed
- * @si: underlying subsystem interface (filled in by opa_core)
+ * @si: subsystem interface (private to opa_core)
  */
 struct opa_core_client {
 	const char *name;
@@ -277,17 +293,25 @@ struct opa_core_client {
 	struct subsys_interface si;
 };
 
-struct hfi_devdata;
-struct opa_core_device *
-opa_core_register_device(struct device *dev, struct opa_core_device_id *id,
-			 struct hfi_devdata *dd, struct opa_core_ops *bus_ops);
-void opa_core_unregister_device(struct opa_core_device *odev);
-
+/**
+ * opa_core_client_register - Register a client with the OPA core
+ * @client: OPA core client
+ *
+ * Once a client has been successfully registered, its "add" callback
+ * will be invoked when an OPA core hardware device is discovered by
+ * the OPA core and its "remove" callback will be invoked when an OPA
+ * core hardware device is removed from the OPA core.
+ *
+ * Return: 0 on success or appropriate error
+ */
 int opa_core_client_register(struct opa_core_client *client);
+
+/**
+ * opa_core_client_unregister - Unregister a client from the OPA core
+ * @client: OPA core client
+ *
+ * Return: none
+ */
 void opa_core_client_unregister(struct opa_core_client *client);
 
-static inline struct opa_core_device *dev_to_opa_core(struct device *dev)
-{
-	return container_of(dev, struct opa_core_device, dev);
-}
 #endif /* _OPA_CORE_H_ */
