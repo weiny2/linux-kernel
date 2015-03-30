2cee864d6aa0b75ac13f65c40ee889ba79f1973a
John Jolly <jjolly@suse.de>
2015-03-30 17:01:05 -0600

    s390/bpf: Fix ALU_NEG (A = -A) (bnc#917125,LTC#121759).
    suse-commit: 4f903c38ff1c3d81208e151badf62762275268d8

--- arch/s390/net/bpf_jit_comp.c
+++ arch/s390/net/bpf_jit_comp.c
@@ -465,8 +465,8 @@ static int bpf_jit_insn(struct bpf_jit *jit, struct sock_filter *filter,
 		EMIT4_DISP(0x88500000, K);
 		break;
 	case BPF_S_ALU_NEG: /* A = -A */
-		/* lnr %r5,%r5 */
-		EMIT2(0x1155);
+		/* lcr %r5,%r5 */
+		EMIT2(0x1355);
 		break;
 	case BPF_S_JMP_JA: /* ip += K */
 		offset = addrs[i + K] + jit->start - jit->prg;
