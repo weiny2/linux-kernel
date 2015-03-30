3a62a37fbb4ccfa16e2a6d99a46e946c21fd561c
John Jolly <jjolly@suse.de>
2015-03-30 17:01:05 -0600

    s390/bpf: Fix JMP_JGE_X (A > X) and JMP_JGT_X (A >= X) (bnc#917125,LTC#121759).
    suse-commit: c256ad09ac0d491099073266fc7c55e439ec5da5

--- arch/s390/net/bpf_jit_comp.c
+++ arch/s390/net/bpf_jit_comp.c
@@ -536,8 +536,8 @@ branch:		if (filter->jt == filter->jf) {
 xbranch:	/* Emit compare if the branch targets are different */
 		if (filter->jt != filter->jf) {
 			jit->seen |= SEEN_XREG;
-			/* cr %r5,%r12 */
-			EMIT2(0x195c);
+			/* clr %r5,%r12 */
+			EMIT2(0x155c);
 		}
 		goto branch;
 	case BPF_S_JMP_JSET_X: /* ip += (A & X) ? jt : jf */
