ae3ebc4d1a18d7ffe8d9ec7756b7cb3847fbbbf2
Mike Marciniszyn <mike.marciniszyn@intel.com>
2015-04-21 17:39:58 -0400

    Update driver version string to 0.7-420

--- common.h
+++ common.h
@@ -186,7 +186,7 @@
  * to the driver itself, not the software interfaces it supports.
  */
 #ifndef HFI_DRIVER_VERSION_BASE
-#define HFI_DRIVER_VERSION_BASE "0.7-394"
+#define HFI_DRIVER_VERSION_BASE "0.7-420"
 #endif
 
 /* create the final driver version string */
