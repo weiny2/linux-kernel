0874336f1299c065142379f37a446860813577b9
Michael Wang <yun.wang@profitbricks.com>
2015-04-28 17:10:19 +0200

    IB/Verbs: Use management helper cap_ib_sa()
    Introduce helper cap_ib_sa() to help us check if the port of an
    IB device support Infiniband Subnet Administration.
    
    Cc: Hal Rosenstock <hal@dev.mellanox.co.il>
    Cc: Steve Wise <swise@opengridcomputing.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
    Cc: Doug Ledford <dledford@redhat.com>
    Cc: Ira Weiny <ira.weiny@intel.com>
    Cc: Sean Hefty <sean.hefty@intel.com>
    Signed-off-by: Michael Wang <yun.wang@profitbricks.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -1834,6 +1834,21 @@ static inline int cap_iw_cm(struct ib_device *device, u8 port_num)
 	return rdma_protocol_iwarp(device, port_num);
 }
 
+/**
+ * cap_ib_sa - Check if the port of device has the capability Infiniband
+ * Subnet Administration.
+ *
+ * @device: Device to be checked
+ * @port_num: Port number of the device
+ *
+ * Return 0 when port of the device don't support Infiniband
+ * Subnet Administration.
+ */
+static inline int cap_ib_sa(struct ib_device *device, u8 port_num)
+{
+	return rdma_protocol_ib(device, port_num);
+}
+
 int ib_query_gid(struct ib_device *device,
 		 u8 port_num, int index, union ib_gid *gid);
 
