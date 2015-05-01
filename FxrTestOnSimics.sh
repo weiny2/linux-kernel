bcccafd1449aa4aa802eb0d5329976e7d1a3b370
Caz Yokoyama <Caz.Yokoyama@intel.com>
2015-05-01 12:38:19 -0400

    add HfiWriteCQ test with minor improvements. Note: HfiWriteCQ is failing on Jenkins. I'll ask Andrew to fix.
    Change-Id: I3983f65710947a0ed3c6f8c43d418028b8d1d0eb
    Reviewed-by: Andrew Friedley <andrew.friedley@intel.com>
    Reviewed-by: Brian Welty <brian.welty@intel.com>
    Reviewed-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Reviewed-by: Sanath Kumar <sanath.s.kumar@intel.com>
    Signed-off-by: Caz Yokoyama <Caz.Yokoyama@intel.com>

--- FxrTestOnSimics.sh
+++ FxrTestOnSimics.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+set -x
 
 fxr=~caz/fabric/fxr
-ssh_cmd="ssh -i /home/caz/.ssh/id_rsa -p4022 root@localhost"
-scp_cmd="scp -i /home/caz/.ssh/id_rsa -P4022"
+ssh_cmd="ssh -p4022 root@localhost"
+scp_cmd="scp -P4022"
 
 # start simics if not yet
 if [ -z `pidof simics-common` ]; then
     pushd ${fxr}/simics/workspace
-    ./simics -no-win -e '$disk_image=../FxrRhel7.craff' knl.simics -e c \
-        >../simics.log &
+    ./simics -no-win -e '$disk_image=../FxrRhel7-201504250757.craff' \
+	FXR.simics >../simics.log &
     popd
     sleep 45
     ${ssh_cmd} "service opa2_hfi start"
@@ -27,15 +28,15 @@ fi
 # update the driver
 ${ssh_cmd} "rm -f /tmp/opa*.x86_64.rpm"
 ${scp_cmd} \
-    /home/caz/rpmbuild/RPMS/x86_64/opa2_hfi-0.0-*.x86_64.rpm \
-    opa-headers.git/opa-headers-0.0-*.x86_64.rpm \
+    ~/rpmbuild/RPMS/x86_64/opa2_hfi-[0-9]*.[0-9]*-[0-9]*.x86_64.rpm \
+    opa-headers.git/opa-headers-[0-9]*.[0-9]*-[0-9]*.x86_64.rpm \
     root@localhost:/tmp
 if [ ! $? ]; then
     echo fail on copying rpm files.
     exit 13
 fi
-${ssh_cmd} "rpm -e opa2_hfi"
-${ssh_cmd} "rpm -e opa-headers"
+${ssh_cmd} "rpm -e opa2_hfi" 2>/dev/null
+${ssh_cmd} "rpm -e opa-headers" 2>/dev/null
 ${ssh_cmd} "rpm -i /tmp/opa*.x86_64.rpm"
 if [ ! $? ]; then
     echo fail on the installation of rpm files.
@@ -48,7 +49,7 @@ if [ ! $? ]; then
     exit 15
 fi
 # run quick test.
-cd ${fxr}/fxr-driver/opa-headers.git/test
+cd opa-headers.git/test
 ./harness.py --nodelist=viper0 --type=quick
 res=$?
 if [ ! ${res} ]; then
