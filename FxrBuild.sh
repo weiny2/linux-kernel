f8a1a3aa992872d6e76e9a5285e45f9ec09ab2d4
Caz Yokoyama <Caz.Yokoyama@intel.com>
2015-05-01 20:40:54 -0400

    rpm file names come from tag.
    Change-Id: Ie1167c7a2e4680baa57711dde98dcaf202cccdd5
    Reviewed-by: Brian Welty <brian.welty@intel.com>
    Reviewed-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Signed-off-by: Caz Yokoyama <Caz.Yokoyama@intel.com>

--- FxrBuild.sh
+++ FxrBuild.sh
@@ -3,9 +3,14 @@
 KERNEL_BUILT_AGAINST=3.12.18-wfr
 RPM_PACKAGE_NAME=opa2_hfi
 VERSION=0.0
+TAG="v[0-9]*.[0-9]*"
 
 # main start here
 rm -rf ~/rpmbuild
+git fetch # acquire newly created tag
+if [ -d .git ]; then
+	VERSION=`git describe --tags --long --match="$TAG" | cut -d'-' -f1 | tr -d 'v'`
+fi
 make rpm KVER=${KERNEL_BUILT_AGAINST} NAME=${RPM_PACKAGE_NAME} VERSION=${VERSION}
 res=$?
 if [ ! ${res} ]; then
@@ -15,6 +20,8 @@ fi
 
 cd opa-headers.git
 rm -f *.x86_64.rpm
+[ -d config ] || mkdir config # eliminate warning
+git fetch # acquire newly created tag
 ./build_rpm.sh
 res=$?
 if [ ! ${res} ]; then
