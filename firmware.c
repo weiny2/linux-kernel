4c4864eb4b654eebb00ec6bdd4c8cdf0d02a9fe4
Dean Luick <dean.luick@intel.com>
2015-05-05 10:13:46 -0700

    IB/hfi1: Turn off simulator 8051 firmware load
    There is no longer benefit or use for loading 8051 firmware
    on the simulator.  The A0 and FPGA platforms now test all the
    interesting code paths.  Turning off the load removes any need
    to coordinate firmware changes on simulator platforms.
    
    INTERNAL: PR 128268 - Open source software will not contain
    INTERNAL: workarounds for red errata since red features must
    INTERNAL: not be exposed in such software
    
    Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Dean Luick <dean.luick@intel.com>

--- firmware.c
+++ firmware.c
@@ -1259,6 +1259,10 @@ int firmware_init(struct hfi_devdata *dd)
 		fw_sbus_load = 0;
 	}
 
+	/* no 8051 on simulator */
+	if (dd->icode == ICODE_FUNCTIONAL_SIMULATOR)
+		fw_8051_load = 0;
+
 	if (!fw_8051_name) {
 		if (dd->icode == ICODE_RTL_SILICON)
 			fw_8051_name = DEFAULT_FW_8051_NAME_ASIC;
