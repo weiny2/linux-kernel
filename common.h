455513ccf0ec26446872fc177f3ec0b638cf570f
Mike Marciniszyn <mike.marciniszyn@intel.com>
2015-05-11 18:14:04 -0400

    Update driver version string to 0.9-52

--- common.h
+++ common.h
@@ -205,7 +205,7 @@
  * to the driver itself, not the software interfaces it supports.
  */
 #ifndef HFI_DRIVER_VERSION_BASE
-#define HFI_DRIVER_VERSION_BASE "0.9-50"
+#define HFI_DRIVER_VERSION_BASE "0.9-52"
 #endif
 
 /* create the final driver version string */
