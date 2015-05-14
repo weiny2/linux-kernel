35c553f8898e5f42e366895474ba16877bd54a6a
Dean Luick <dean.luick@intel.com>
2015-05-14 05:43:16 -0700

    IB/hfi1: Remove another security reference, sbus read
    Remove another security comment.
    Remove unused SBus read define.
    
    INTERNAL: PR 128268 - Open source software will not contain
    INTERNAL: workarounds for red errata since red features must
    INTERNAL: not be exposed in such software
    
    Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Dean Luick <dean.luick@intel.com>

--- firmware.c
+++ firmware.c
@@ -439,7 +439,7 @@ static int obtain_one_firmware(struct hfi1_devdata *dd, const char *name,
 						sizeof(struct firmware_file);
 			/*
 			 * Header does not include r2 and mu - generate here.
-			 * For now, fail if validating.
+			 * For now, fail.
 			 */
 			dd_dev_err(dd, "driver is unable to validate firmware without r2 and mu (not in firmware file)\n");
 			ret = -EINVAL;
@@ -826,7 +826,6 @@ static int load_8051_firmware(struct hfi1_devdata *dd,
 	 */
 	/*
 	 * Firmware load step 6.  Set MISC_CFG_FW_CTRL.FW_8051_LOADED
-	 * Clear or set DISABLE_VALIDATION depending on if we are validating.
 	 */
 	write_csr(dd, MISC_CFG_FW_CTRL, MISC_CFG_FW_CTRL_FW_8051_LOADED_SMASK);
 
