fa566304d91270449032ae0fae0a4b66e65c0383
Benjamin Poirier <bpoirier@suse.com>
2015-07-09 20:43:20 -0700

    vmxnet3: Bump up driver version number (bsc#936423).
    suse-commit: bbdb9330d1190a5b823054af5c797bfd837f3627

--- drivers/net/vmxnet3/vmxnet3_int.h
+++ drivers/net/vmxnet3/vmxnet3_int.h
@@ -70,10 +70,10 @@
 /*
  * Version numbers
  */
-#define VMXNET3_DRIVER_VERSION_STRING   "1.2.0.0-k"
+#define VMXNET3_DRIVER_VERSION_STRING   "1.4.2.0-k"
 
 /* a 32-bit int, each byte encode a verion number in VMXNET3_DRIVER_VERSION */
-#define VMXNET3_DRIVER_VERSION_NUM      0x01020000
+#define VMXNET3_DRIVER_VERSION_NUM      0x01040200
 
 #if defined(CONFIG_PCI_MSI)
 	/* RSS only makes sense if MSI-X is supported. */
