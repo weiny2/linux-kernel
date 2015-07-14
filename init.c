e8fb10bb1a4ddea80d2b55c63dd879a715ac66d7
Dean Luick <dean.luick@intel.com>
2015-07-14 15:59:45 -0700

    IB/hfi1:  Correctly check header queue size
    The header queue size must be a multiple of HDRQ_INCREMENT.
    
    INTERNAL: PR 128063 - STL1 A0 Verbs RDMA Uni-directional Bandwidth
    INTERNAL: for 1MB message size is lower than the STL1 Verbs LZ Target
    
    Reviewed-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Signed-off-by: Dean Luick <dean.luick@intel.com>

--- init.c
+++ init.c
@@ -253,7 +253,7 @@ struct hfi1_ctxtdata *hfi1_create_ctxtdata(struct hfi1_pportdata *ppd, u32 ctxt)
 		rcd->eager_base = base * dd->rcv_entries.group_size;
 
 		/* Validate and initialize Rcv Hdr Q variables */
-		if (rcvhdrcnt & HDRQ_INCREMENT) {
+		if (rcvhdrcnt % HDRQ_INCREMENT) {
 			dd_dev_err(dd,
 				   "ctxt%u: header queue count %d must be divisible by %d\n",
 				   rcd->ctxt, rcvhdrcnt, HDRQ_INCREMENT);
