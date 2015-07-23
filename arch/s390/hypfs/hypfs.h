cf160c11a2683d6313572382eb69c37487b02446
John Jolly <jjolly@suse.de>
2015-07-23 07:37:24 -0600

    s390/hypfs: Eliminate hypfs interval (bsc#934400,FATE#318070,LTC#KRN1311).
    suse-commit: fbdae91c61ef3c722404de951aa5a1b6d9b0ebd1

--- arch/s390/hypfs/hypfs.h
+++ arch/s390/hypfs/hypfs.h
@@ -48,7 +48,6 @@ struct hypfs_dbfs_data {
 	void			*buf_free_ptr;
 	size_t			size;
 	struct hypfs_dbfs_file	*dbfs_file;
-	struct kref		kref;
 };
 
 struct hypfs_dbfs_file {
@@ -58,8 +57,6 @@ struct hypfs_dbfs_file {
 	void		(*data_free)(const void *buf_free_ptr);
 
 	/* Private data for hypfs_dbfs.c */
-	struct hypfs_dbfs_data	*data;
-	struct delayed_work	data_free_work;
 	struct mutex		lock;
 	struct dentry		*dentry;
 };
