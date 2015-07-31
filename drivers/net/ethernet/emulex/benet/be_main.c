14185017d6ded3b6b6140024071e2f9eb8d56d2e
David Chang <dchang@suse.com>
2015-07-31 11:56:57 +0800

    be2net: use adapter->flags to track SRIOV state (bsc#921784 FATE#318561).
    suse-commit: 599b19409062980f9dea5b7eab14b44d134582ae

--- drivers/net/ethernet/emulex/benet/be_main.c
+++ drivers/net/ethernet/emulex/benet/be_main.c
@@ -3044,6 +3044,7 @@ static void be_vf_clear(struct be_adapter *adapter)
 done:
 	kfree(adapter->vf_cfg);
 	adapter->num_vfs = 0;
+	adapter->flags &= ~BE_FLAGS_SRIOV_ENABLED;
 }
 
 static void be_clear_queues(struct be_adapter *adapter)
@@ -3236,6 +3237,8 @@ static int be_vf_setup(struct be_adapter *adapter)
 			goto err;
 		}
 	}
+
+	adapter->flags |= BE_FLAGS_SRIOV_ENABLED;
 	return 0;
 err:
 	dev_err(dev, "VF setup failed\n");
