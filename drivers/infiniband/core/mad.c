e7f579eaac72aafd484ed29d866e6809ce49d371
Ira Weiny <ira.weiny@intel.com>
2015-08-06 09:45:14 -0400

    IB/mad: remove annoying build warnings
    These have all been addressed in the upstream patches.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- drivers/infiniband/core/mad.c
+++ drivers/infiniband/core/mad.c
@@ -758,7 +758,6 @@ static int handle_outgoing_dr_smp(struct ib_mad_agent_private *mad_agent_priv,
 	 * hop_ptr or hop_cnt.  See section 14.2.2, Vol 1 IB spec.
 	 */
 	if (smp->base_version == JUMBO_MGMT_BASE_VERSION) {
-#warning "FIXME there needs to be a selection for the STL SM Class version here"
 		if ((stl_get_smp_direction(stl_smp)
 		     ? stl_smp->route.dr.dr_dlid : stl_smp->route.dr.dr_slid) ==
 		     STL_LID_PERMISSIVE &&
@@ -3300,7 +3299,6 @@ static void ib_mad_init_device(struct ib_device *device)
 {
 	int start, end, i;
 
-#warning "FIXME for upstream: allow STL transport here as well"
 	if (rdma_node_get_transport(device->node_type) != RDMA_TRANSPORT_IB)
 		return;
 
