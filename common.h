aedd25eed433a8f48d23ade008321c4af963403c
Mike Marciniszyn <mike.marciniszyn@intel.com>
2015-08-14 18:25:56 -0400

    Update driver version string to 0.9-278

--- common.h
+++ common.h
@@ -204,7 +204,7 @@
  * to the driver itself, not the software interfaces it supports.
  */
 #ifndef HFI1_DRIVER_VERSION_BASE
-#define HFI1_DRIVER_VERSION_BASE "0.9-265"
+#define HFI1_DRIVER_VERSION_BASE "0.9-278"
 #endif
 
 /* create the final driver version string */
