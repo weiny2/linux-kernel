14d0a33018c861c950fcd3456b4483ad85058621
Johannes Thumshirn <jthumshirn@suse.de>
2015-09-01 16:31:30 +0200

    hpsa: define extended_report_lun_entry data structure.
    suse-commit: 77f85534b9ff00c87886f6878e597a32185d6f37

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -2879,26 +2879,20 @@ static int hpsa_get_pdisk_of_ioaccel2(struct ctlr_info *h,
 	nphysicals = be32_to_cpu(*((__be32 *)physicals->LUNListLength)) /
 							responsesize;
 
-
 	/* find ioaccel2 handle in list of physicals: */
 	for (i = 0; i < nphysicals; i++) {
+		struct ext_report_lun_entry *entry = &physicals->LUN[i];
+
 		/* handle is in bytes 28-31 of each lun */
-		if (memcmp(&((struct ReportExtendedLUNdata *)
-				physicals)->LUN[i][20], &find, 4) != 0) {
+		if (entry->ioaccel_handle != find)
 			continue; /* didn't match */
-		}
 		found = 1;
-		memcpy(scsi3addr, &((struct ReportExtendedLUNdata *)
-					physicals)->LUN[i][0], 8);
+		memcpy(scsi3addr, entry->lunid, 8);
 		if (h->raid_offload_debug > 0)
 			dev_info(&h->pdev->dev,
-				"%s: Searched h=0x%08x, Found h=0x%08x, scsiaddr 0x%02x%02x%02x%02x%02x%02x%02x%02x\n",
+				"%s: Searched h=0x%08x, Found h=0x%08x, scsiaddr 0x%8phN\n",
 				__func__, find,
-				((struct ReportExtendedLUNdata *)
-					physicals)->LUN[i][20],
-				scsi3addr[0], scsi3addr[1], scsi3addr[2],
-				scsi3addr[3], scsi3addr[4], scsi3addr[5],
-				scsi3addr[6], scsi3addr[7]);
+				entry->ioaccel_handle, scsi3addr);
 		break; /* found it */
 	}
 
@@ -2968,8 +2962,8 @@ static int hpsa_gather_lun_info(struct ctlr_info *h,
 
 static u8 *figure_lunaddrbytes(struct ctlr_info *h, int raid_ctlr_position,
 	int i, int nphysicals, int nlogicals,
-	void *physdev_list, struct ReportLUNdata *logdev_list,
-	int physical_mode)
+	struct ReportExtendedLUNdata *physdev_list,
+	struct ReportLUNdata *logdev_list, int physical_mode)
 {
 	/* Helper function, figure out where the LUN ID info is coming from
 	 * given index i, lists of physical and logical devices, where in
@@ -2985,16 +2979,9 @@ static u8 *figure_lunaddrbytes(struct ctlr_info *h, int raid_ctlr_position,
 	if (i == raid_ctlr_position)
 		return RAID_CTLR_LUNID;
 
-	if (i < logicals_start) {
-		const int index = i - (raid_ctlr_position == 0);
-		if (physical_mode) {
-			struct ReportExtendedLUNdata *p = physdev_list;
-			return &p->LUN[index][0];
-		} else {
-			struct ReportLUNdata *p = physdev_list;
-			return &p->LUN[index][0];
-		}
-	}
+	if (i < logicals_start)
+		return &physdev_list->LUN[i -
+					(raid_ctlr_position == 0)].lunid[0];
 	if (i < last_device)
 		return &logdev_list->LUN[i - nphysicals -
 			(raid_ctlr_position == 0)][0];
