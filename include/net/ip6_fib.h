802d7368c29e1cc8e139e101d2d9c112cd6a5118
Kernel Build Daemon <kbuild@suse.de>
2015-10-08 07:03:02 +0200

    Delete patches.kabi/* workarounds
    suse-commit: d2ab76f61fbab6a135e39c998f7d9d1cf36357f1

--- include/net/ip6_fib.h
+++ include/net/ip6_fib.h
@@ -120,14 +120,13 @@ struct rt6_info {
 	struct rt6key			rt6i_src;
 	struct rt6key			rt6i_prefsrc;
 	u32				rt6i_metric;
-#ifndef __GENKSYMS__
-	u32				rt6i_pmtu;
-#endif
 
 	struct inet6_dev		*rt6i_idev;
 	unsigned long			_rt6i_peer;
 
 	u32				rt6i_genid;
+
+	u32				rt6i_pmtu;
 	/* more non-fragment space at head required */
 	unsigned short			rt6i_nfheader_len;
 
