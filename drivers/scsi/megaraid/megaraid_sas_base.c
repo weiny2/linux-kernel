e14e6206c59790d579229312e73f3fbb2708aeec
Johannes Thumshirn <jthumshirn@suse.de>
2015-10-08 13:29:20 +0200

    Reenable call scsi_activate_tcq() per SCSi target and enabled tagged_support inside struct scsi_device (bsc#922632).
    suse-commit: 3011bcbae71e7c7be6dcb571c16a9237e2eaa383

--- drivers/scsi/megaraid/megaraid_sas_base.c
+++ drivers/scsi/megaraid/megaraid_sas_base.c
@@ -1697,10 +1697,14 @@ static int megasas_slave_alloc(struct scsi_device *sdev)
 			sdev->id;
 		if (instance->pd_list[pd_index].driveState ==
 					MR_PD_STATE_SYSTEM) {
-			return 0;
+			goto scan_target;
 		}
 		return -ENXIO;
 	}
+scan_target:
+	sdev->tagged_supported = 1;
+	scsi_activate_tcq(sdev, sdev->queue_depth);
+
 	return 0;
 }
 
