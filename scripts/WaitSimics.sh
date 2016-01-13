e83e363c615d773c41bbe78934c9e0bee154ff2c
Caz Yokoyama <caz.yokoyama@intel.com>
2016-01-13 15:07:50 -0800

    STL-4160 bullseye code coverage on Simics
    usage: scripts/FxrBullseye.sh [quick|default]
    
    Obtain code coverage data and start its browser.
    Invoke Simics if it does not run yet.
    Default test mode is quick.
    Driver source tree and code coverage data are located in /tmp. So that
    move to /tmp/fxr-driver, point COVFILE to tmp/fxr-driver/cov.test
    and start CoverageBrowser.
    
    Change-Id: I817efe879276b9ee24e3a13ba741784ce4221073
    Reviewed-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Reviewed-by: Brian Welty <brian.welty@intel.com>
    Reviewed-by: Sanath Kumar <sanath.s.kumar@intel.com>
    Signed-off-by: Caz Yokoyama <caz.yokoyama@intel.com>

---
+++ scripts/WaitSimics.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+xtrace_state=`set -o | grep xtrace | sed -e"s/ *//g" | sed -e"s/\t/ /" | cut -d" " -f2`
+set +x
+
+. scripts/GlobalDefinition.sh
+
+declare -i iterate=0
+
+while [ -z `pidof simics-common` ]; do
+	if [ $((iterate++)) -gt ${MAX_SIMICS_WAIT} ]; then
+		printf "No simics process for %d seconds\n" ${MAX_SIMICS_WAIT}
+		exit 1
+	fi
+	sleep 1
+done
+
+ssh_cmd="ssh -p${viper0} root@localhost -oConnectTimeout=1"
+
+while ! ${ssh_cmd} ls 2>/dev/null >/dev/null; do
+	if [ $((iterate++)) -gt ${MAX_SIMICS_WAIT} ]; then
+		printf "No simics process for %d seconds\n" ${MAX_SIMICS_WAIT}
+		exit 1
+	fi
+	sleep 1
+done
+
+if [ ${xtrace_state} == "on" ]; then
+	set -x
+fi
---
+++ scripts/bullseye_define.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export BULLSEYE_SRC_DIR=/mnt/fabric
+export BULLSEYE_DIR=/opt/BullseyeCoverage
+export HOME_DIR=/home/fxr
+export BULLSEYE_WORK=${HOME_DIR}/fxr-driver
+export COVFILE=${BULLSEYE_WORK}/cov.test
+export SSH_CMD="ssh -p${viper0} root@localhost"
