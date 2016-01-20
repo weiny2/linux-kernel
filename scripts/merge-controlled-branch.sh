01f23c5e15b718e08029361a7f162ac22a36c525
Mike Marciniszyn <mike.marciniszyn@intel.com>
2016-01-20 13:21:22 -0500

    IB/hfi1/scripts: generalize merge script
    This script now accepts a support branch name as an
    option (default 10_0-branch) and automatically
    determines branches to process.
    
    INTERNAL: PR 127361 - Update driver to the latest upstream code base
    
    Reviewed-by: Jubin John <jubin.john@intel.com>
    Signed-off-by: Mike Marciniszyn <mike.marciniszyn@intel.com>

--- scripts/merge-controlled-branch.sh
+++ scripts/merge-controlled-branch.sh
@@ -1,35 +1,40 @@
 #!/bin/sh
 #
-# TODO - make these args
-distros="3.12 rhel7.1 sles12.0"
-sourcebranch=beta0
+
+getbranches()
+{
+	git branch -r | grep "$sourcebranch$" | grep wfr-ifs | xargs basename -a
+}
+
+sourcebranch=10_0-branch
+while getopts "s:" opt
+do
+        case $opt in
+        s)
+		sourcebranch=$OPTARG
+                ;;
+        *)
+                echo "Usage: `basename $0` [ -s sourcebranch ]"
+                exit 1
+                ;;
+        esac
+done
+
+
 rm -rf wfr-driver-controlled-merge
 git clone ssh://$USER@git-amr-2.devtools.intel.com:29418/wfr-driver.git wfr-driver-controlled-merge
 cd wfr-driver-controlled-merge
-dir=$PWD
-for distro in $distros
+branches=`getbranches`
+for branch in $branches
 do
-	# special case
-	if [ "$distro" != "3.12" ]
-	then
-		git checkout -b wfr-ifs-${distro}-${sourcebranch} origin/wfr-ifs-${distro}-${sourcebranch}
-		if ! git merge --log -m"merge from $sourcebranch to wfr-ifs-${distro}-${sourcebranch}" origin/$sourcebranch
-		then
-			echo "merge failed\n"
-			exit 1
-		else
-			echo "ready for git push origin wfr-ifs-${distro}-${sourcebranch}"
-			/bin/sh || exit 1
-		fi
-	fi
-	git checkout -b for-wfr-ifs-${distro}-${sourcebranch} origin/for-wfr-ifs-${distro}-${sourcebranch}
-	if ! git merge --log -m"merge from $sourcebranch to for-wfr-ifs-${distro}-${sourcebranch}" origin/$sourcebranch
+	git checkout -b $branch origin/$branch
+	if ! git merge --log -m"merge from $sourcebranch to $branch" origin/$sourcebranch
 	then
 		echo "merge failed - resolve conflicts and push\n"
-		/bin/sh
+		/bin/sh || exit 1
 	else
 		
-		echo "ready for git push origin for-wfr-ifs-${distro}-${sourcebranch}"
+		echo "ready for git push to origin $branch"
 		/bin/sh || exit 1
 	fi
 done
