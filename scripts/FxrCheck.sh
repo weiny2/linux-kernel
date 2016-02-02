d7394878fcca5e76c74b18c756acb4074db128aa
Caz Yokoyama <caz.yokoyama@intel.com>
2016-02-02 12:05:47 -0800

    STL-5429 integrate FxrCheckpatch.sh to FxrCheck.sh
    Change-Id: Ic702802109ebeadaf9774244948681baca3ed689
    Reviewed-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Reviewed-by: Brian Welty <brian.welty@intel.com>
    Reviewed-by: Sanath Kumar <sanath.s.kumar@intel.com>
    Signed-off-by: Caz Yokoyama <caz.yokoyama@intel.com>

--- scripts/FxrCheck.sh
+++ scripts/FxrCheck.sh
@@ -5,6 +5,16 @@
 myname=$0
 PWD=`pwd`
 
+# checkpatch
+DIRS="opa2 opa2_vnic opa_core user verbs"
+CHECKPATCH_COMMAND=/lib/modules/${KERNEL_VERSION}/build/scripts/checkpatch.pl
+CHECKPATCH_OPTIONS="--file --no-tree --terse --strict"
+
+find ${DIRS} -type f -name "*.h" -o -name "*.c" |\
+while read file; do
+	${CHECKPATCH_COMMAND} ${CHECKPATCH_OPTIONS} ${file}
+done # just report
+
 # sparse
 export PATH=$PATH:/usr/local/bin
 # check whether sparse is installed
