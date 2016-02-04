73cd1c62d3a1c49a5a3f2c677fccd3aa6f16ec98
Caz Yokoyama <caz.yokoyama@intel.com>
2016-02-04 14:28:41 -0800

    STL-5429 use FxrInstall.sh
    Change-Id: I4a869af537abd39e41e5d540e005a6efb86262a3
    Signed-off-by: Caz Yokoyama <caz.yokoyama@intel.com>

--- scripts/FxrTestOnSimics.sh
+++ scripts/FxrTestOnSimics.sh
@@ -52,9 +52,6 @@ shift $(($OPTIND - 1))
 ByJenkins=no
 [ `id -un` == root ] && pwd | grep --quiet jenkins && ByJenkins=yes
 
-. scripts/StartSimics.sh
-. scripts/WaitSimics.sh
-
 # show which version/commit of Simics, craff file and others I am using
 pwd; git log -n1 | head -1
 ${fxr}/simics/workspace/bin/simics --version
@@ -62,37 +59,7 @@ ${fxr}/simics/workspace/bin/simics --version
 ls -l ${fxr}/simics/FxrRhel7.craff
 ls -l ${fxr}/simics/SynopsisInstructionSetSimulator
 
-ssh -p${viper0} root@localhost "service --skip-redirect opafm stop"
-
-for viper in ${viper0} ${viper1}; do
-    ssh_cmd="ssh -p${viper} root@localhost"
-    scp_cmd="scp -P${viper}"
-
-    ${ssh_cmd} "dmesg -c > /dev/null"
-    # stop opa2_hfi daemon to release the driver
-    ${ssh_cmd} "service --skip-redirect opa2_hfi stop"
-    if [ ! $? ]; then
-	echo fail on stoping opa2_hfi.
-	exit 12
-    fi
-    # update the driver
-    ${ssh_cmd} "rm -f ${tmp_dir}/opa*.x86_64.rpm"
-    ${scp_cmd} \
-	rpmbuild/RPMS/x86_64/opa2_hfi-[0-9]*.[0-9]*-[0-9]*.x86_64.rpm \
-	opa-headers.git/opa-headers-[0-9]*.[0-9]*-[0-9]*.x86_64.rpm \
-	root@localhost:${tmp_dir}
-    if [ ! $? ]; then
-	echo fail on copying rpm files.
-	exit 13
-    fi
-    ${ssh_cmd} "rpm -e opa2_hfi" 2>/dev/null
-    ${ssh_cmd} "rpm -e opa-headers" 2>/dev/null
-    ${ssh_cmd} "rpm -i ${tmp_dir}/opa*.x86_64.rpm"
-    if [ ! $? ]; then
-	echo fail on the installation of rpm files.
-	exit 14
-    fi
-done
+. scripts/FxrInstall.sh
 
 if [ $only_install ] && ! [ $only_load_driver ]; then
 	exit 0
