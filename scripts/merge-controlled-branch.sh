23937305ab37c32854d00088913fd6931cb799b3
Jubin John <jubin.john@intel.com>
2016-03-11 17:02:42 -0800

    IB/hfi1/scripts: Improve controlled merge script
    Improve the controlled merge script with the following enhancements:
    1. Automated driver version edit
    2. Skip ddn branches in branch selection
    3. Complete all branch merges before breaking out for inspection
    
    INTERNAL: PR 125486 - Improve driver regression testing
    
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Jubin John <jubin.john@intel.com>
    Change-Id: I1c00a3a4ffc6077ca845b656066baa41f7c1e500

--- scripts/merge-controlled-branch.sh
+++ scripts/merge-controlled-branch.sh
@@ -3,7 +3,7 @@
 
 getbranches()
 {
-	git branch -r | grep "$sourcebranch$" | grep wfr-ifs | xargs basename -a
+	git branch -r | grep "$sourcebranch$" | grep wfr-ifs | grep -v ddn | xargs basename -a
 }
 
 sourcebranch=10_0-branch
@@ -22,19 +22,26 @@ done
 
 
 rm -rf wfr-driver-controlled-merge
-git clone ssh://$USER@git-amr-2.devtools.intel.com:29418/wfr-driver.git wfr-driver-controlled-merge
+git clone --branch $sourcebranch ssh://$USER@git-amr-2.devtools.intel.com:29418/wfr-driver.git wfr-driver-controlled-merge
 cd wfr-driver-controlled-merge
+
+# Automated driver version edit
+mversion=`make version`
+sed -i "/^#define HFI1_DRIVER_VERSION_BASE/s/\".*\"/\"$mversion\"/" common.h
+git commit -s -m"Update driver version string to ${mversion}" common.h
+
+# Do all branch merges, then breakout for inspection
 branches=`getbranches`
 for branch in $branches
 do
 	git checkout -b $branch origin/$branch
-	if ! git merge --log -m"merge from $sourcebranch to $branch" origin/$sourcebranch
+	if ! git merge --log -m"merge from $sourcebranch to $branch" $sourcebranch
 	then
 		echo "merge failed - resolve conflicts and push\n"
 		/bin/sh || exit 1
-	else
-		
-		echo "ready for git push to origin $branch"
-		/bin/sh || exit 1
 	fi
 done
+
+echo -e "\nAll merges done. Check branches (git branch) and remove any unnecessary branches (git branch -D branchname)"
+echo -e "Ready for: git push --all"
+/bin/sh || exit 1
