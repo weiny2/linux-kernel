9df6d538e2a4090fa89f69f821ae0abfa2d14b13
Caz Yokoyama <caz.yokoyama@intel.com>
2016-03-22 16:10:18 -0700

    STL-7281 integrate hfidiags into nightly Jenkins build of rpms
    Change-Id: I08cb3e32c053656f9c4d918fce2f52cff71c4778
    Reviewed-by: Niranjana Vishwanathapura <niranjana.vishwanathapura@intel.com>
    Reviewed-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Reviewed-by: Brian Welty <brian.welty@intel.com>
    Reviewed-by: Sanath Kumar <sanath.s.kumar@intel.com>
    Signed-off-by: Caz Yokoyama <caz.yokoyama@intel.com>

--- scripts/GlobalDefinition.sh
+++ scripts/GlobalDefinition.sh
@@ -3,3 +3,7 @@ KERNEL_VERSION=4.5.0-rc5+
 # Am I invoked by Jenkins?
 export ByJenkins=no
 [ `id -un` == root ] && pwd | grep --quiet jenkins && ByJenkins=yes
+
+# for hfidiag
+JENKINS_WORKSPACE=/mnt/fabric/jenkins/workspace
+DIAGTOOL_REPO=wfr-diagtools-sw
