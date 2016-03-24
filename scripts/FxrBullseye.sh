6421a0afa990243cd098b995926ad39342e85e10
Caz Yokoyama <caz.yokoyama@intel.com>
2016-03-24 11:37:41 -0700

    STL-6490 Take code coverage data from both viper0 and viper1 and merge them
    Change-Id: I81a52cdae9e238a879eac3d60f2d1b5406579142
    Reviewed-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Reviewed-by: Brian Welty <brian.welty@intel.com>
    Reviewed-by: Sanath Kumar <sanath.s.kumar@intel.com>
    Signed-off-by: Caz Yokoyama <caz.yokoyama@intel.com>

--- scripts/FxrBullseye.sh
+++ scripts/FxrBullseye.sh
@@ -18,83 +18,98 @@ COVFILE=${BULLSEYE_WORK}/cov.${test_type}
 
 . scripts/FxrInstall.sh
 
-# update date and time to eliminate the warning, "..has modification time ...in the future"
-${SSH_CMD} "date `date +%m%d%H%M%y`"
+for viper in ${viper0} ${viper1}; do
+	SSH_CMD="ssh -p${viper} root@localhost"
+	# update date and time to eliminate the warning, "..has modification time ...in the future"
+	${SSH_CMD} "date `date +%m%d%H%M%y`"
 
-# clean up
-rm -rf cov.test /tmp/fxr-driver
-${SSH_CMD} "\
-	rm -rf ${BULLSEYE_WORK}; \
-	cd /opt; rm -rf BullseyeCoverage; \
-	rm -rf .BullseyeCoverage; \
-	dmesg -C; # clean up dmesg \
-"
+	# clean up
+	rm -rf cov.test /tmp/fxr-driver
+	${SSH_CMD} "
+		rm -rf ${BULLSEYE_WORK}
+		cd /opt; rm -rf BullseyeCoverage
+		rm -rf .BullseyeCoverage
+		dmesg -C; # clean up dmesg
+	"
 
-# make sure to unload bullseye kernel module
-${SSH_CMD} "if lsmod | grep -q cov; then rmmod libcov_lkm; fi"
+	# make sure to unload bullseye kernel module
+	${SSH_CMD} "if lsmod | grep -q cov; then rmmod libcov_lkm; fi"
 
-# copy /opt/BullseyeCoverage
-pushd ${BULLSEYE_SRC_DIR}
-tar cf - BullseyeCoverage | ${SSH_CMD} "cd /opt; tar xf -" 2>/dev/null
-popd
+	# copy /opt/BullseyeCoverage
+	pushd ${BULLSEYE_SRC_DIR}
+		tar cf - BullseyeCoverage | ${SSH_CMD} "cd /opt; tar xf -" 2>/dev/null
+	popd
 
-# transfer driver source code.
-git submodule update
-pushd ..
-tar cf - \
-	--exclude=".*" \
-	--exclude="rpmbuild" \
-	--exclude="scripts" \
-	--exclude="opa2_hfi*" \
-	--exclude="cov.*" \
-	${CURRENT_DIR} | \
-	${SSH_CMD} "cd ${HOME_DIR}; tar xf -" 2>/dev/null
-popd
+	# transfer driver source code.
+	git submodule update
+	pushd ..
+		tar cf - \
+			--exclude=".*" \
+			--exclude="rpmbuild" \
+			--exclude="scripts" \
+			--exclude="opa2_hfi*" \
+			--exclude="cov.*" \
+			--exclude="cscope.out" \
+			${CURRENT_DIR} | \
+			${SSH_CMD} "cd ${HOME_DIR}; tar xf -" 2>/dev/null
+	popd
 
-# build and install bullseye kernel module and driver
-${SSH_CMD} "\
-	sed -i -e'/^export COVFILE=/d' /root/.bashrc; \
-	echo export COVFILE=${COVFILE} >>/root/.bashrc
-	export COVFILE=${COVFILE}; \
-	${BULLSEYE_DIR}/bin/cov01 -1; \
-	${BULLSEYE_DIR}/bin/covselect -q --deleteAll; \
-	${BULLSEYE_DIR}/bin/covselect -q --add ./; \
-	${BULLSEYE_DIR}/bin/covselect -q --add '!hfi2_vnic/vnic.git/opa_vnic/opa_vnic_debug.c'; \
-	${BULLSEYE_DIR}/bin/covselect -q --add '!opa-headers.git/test/'
-	${BULLSEYE_DIR}/bin/covselect -q --add '!kfi/kfi_main.c'
-	${BULLSEYE_DIR}/bin/covselect -q --add '!hfi2/snoop.c'
-	cd ${BULLSEYE_DIR}/run/linuxKernel; \
-	make -C /lib/modules/\`uname -r\`/build M=\`pwd\`; \
-	insmod ${BULLSEYE_DIR}/run/linuxKernel/libcov-lkm.ko; \
-	grep -q cov_ /lib/modules/\`uname -r\`/build/Module.symvers || \
-		cat Module.symvers >>/lib/modules/\`uname -r\`/build/Module.symvers; \
-	cd ${BULLSEYE_WORK}/opa-headers.git; \
-	./autogen.sh; \
-	./configure; \
-	echo '--symbolic' >${BULLSEYE_DIR}/bin/covc.cfg
-	export PATH=${BULLSEYE_DIR}/bin:${PATH}; \
-	cd ${BULLSEYE_WORK}; \
-	make -C /lib/modules/\`uname -r\`/build M=\`pwd\`; \
-	cp \`find . -name "*.ko"\` /lib/modules/\`uname -r\`/updates; \
-	make -C opa-headers.git; \
-	make -C opa-headers.git install; \
-"
+	# build and install bullseye kernel module and driver
+	${SSH_CMD} "\
+		sed -i -e'/^export COVFILE=/d' /root/.bashrc
+		echo export COVFILE=${COVFILE} >>/root/.bashrc
+		export COVFILE=${COVFILE}
+		${BULLSEYE_DIR}/bin/cov01 -1
+		${BULLSEYE_DIR}/bin/covselect -q --deleteAll
+		${BULLSEYE_DIR}/bin/covselect -q --add ./
+		${BULLSEYE_DIR}/bin/covselect -q --add '!hfi2_vnic/vnic.git/opa_vnic/opa_vnic_debug.c'
+		${BULLSEYE_DIR}/bin/covselect -q --add '!opa-headers.git/test/'
+		${BULLSEYE_DIR}/bin/covselect -q --add '!kfi/kfi_main.c'
+		${BULLSEYE_DIR}/bin/covselect -q --add '!hfi2/snoop.c'
+		cd ${BULLSEYE_DIR}/run/linuxKernel
+		make -C /lib/modules/\`uname -r\`/build M=\`pwd\`
+		insmod ${BULLSEYE_DIR}/run/linuxKernel/libcov-lkm.ko
+		grep -q cov_ /lib/modules/\`uname -r\`/build/Module.symvers ||
+			cat Module.symvers >>/lib/modules/\`uname -r\`/build/Module.symvers
+		cd ${BULLSEYE_WORK}/opa-headers.git
+		./autogen.sh
+		./configure
+		echo '--symbolic' >${BULLSEYE_DIR}/bin/covc.cfg
+		export PATH=${BULLSEYE_DIR}/bin:${PATH}
+		cd ${BULLSEYE_WORK}
+		make -C /lib/modules/\`uname -r\`/build M=\`pwd\`
+		cp \`find . -name "*.ko"\` /lib/modules/\`uname -r\`/updates
+		make -C opa-headers.git
+		make -C opa-headers.git install
+	"
+done
+
 # run harness
 pushd opa-headers.git/test
 ./harness.py --nodelist=viper0,viper1 --type=${test_type}
 popd
 
-# wrap up bullseye
-${SSH_CMD} "\
-	service --skip-redirect opafm stop; \
-	export COVFILE=${COVFILE}; ${BULLSEYE_DIR}/bin/covgetkernel; \
-	service --skip-redirect opa2_hfi stop; \
-	rmmod libcov-lkm; \
-	${BULLSEYE_DIR}/bin/cov01 -0; \
-"
+# stop FM to prepare unloading the driver
+SSH_CMD="ssh -p${viper0} root@localhost"
+${SSH_CMD} "service --skip-redirect opafm stop"
 
-# transfer to host
-${SSH_CMD} "cat ${COVFILE}" >cov.${test_type}
+# wrap up bullseye
+for viper in ${viper0} ${viper1}; do
+	SSH_CMD="ssh -p${viper} root@localhost"
+	${SSH_CMD} "
+		export COVFILE=${COVFILE}; ${BULLSEYE_DIR}/bin/covgetkernel
+		service --skip-redirect opa2_hfi stop
+		rmmod libcov-lkm
+		${BULLSEYE_DIR}/bin/cov01 -0
+	"
+	# transfer to host
+	${SSH_CMD} "cat ${COVFILE}" >cov.${test_type}.${viper}
+done
+
+# merge 2 covfiles into one, .e.g. cov.quick.4022 cov.quick.5022 -> cov.quick
+export COVFILE=`pwd`/cov.${test_type}
+${BULLSEYE_DIR}/bin/covmerge --create cov.${test_type}.*
+rm -f cov.${test_type}.*
 
 # to view code coverage result
 # export COVFILE=`pwd`/cov.${test_type}
