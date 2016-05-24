weiny2/linux-kernel

commit 2c607e07a003bd0bbc84395030e303524e2c0381

IB/IPoIB: More changes to support extended LIDs in IPOIB
1. IPOIB is updated to save the extended SLID and DLID information
that comes in the path record response before changing the GIDs in
the path record to the IB GID format.
2. IPOIB CM checks the saved slid and dlid. If they are extended,
it creates OPA GIDs from these LIDs and uses them for connection
request.

Change-Id: Ibb8c2205d78d1840856cda3c45722f08f432b43b
Signed-off-by: Don Hiatt <don.hiatt@intel.com>
Signed-off-by: Dasaratharaman Chandramouli <dasaratharaman.chandramouli@intel.com>
Reviewed-by: Ira Weiny <ira.weiny@intel.com>
Reviewed-by: Ashutosh Dixit <ashutosh.dixit@intel.com>
Reviewed-by: John Fleck <john.fleck@intel.com>

drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -718,20 +718,31 @@ static void path_rec_completion(int status,
 	spin_lock_irqsave(&priv->lock, flags);
 
 	if (!IS_ERR_OR_NULL(ah)) {
-		if (rdma_cap_opa_ah(priv->ca, priv->port)) {
-			/**
-			 * Extended LIDs might get programmed into GIDs in the
-			 * case of OPA devices. Since we have created the ah
-			 * above which would have made use of the lids, now is
-			 * a good time to change them back to regular GIDs
-			 */
-			if (ib_is_opa_gid(&pathrec->sgid))
-				pathrec->sgid = path->pathrec.sgid;
-			if (ib_is_opa_gid(&pathrec->dgid))
-				pathrec->dgid = path->pathrec.dgid;
-			ipoib_dbg(priv, "PathRec SGID %pI6 DGID %pI6\n",
-				  pathrec->sgid.raw, pathrec->dgid.raw);
+		/**
+		 * Extended LIDs might get programmed into GIDs in the
+		 * case of OPA devices. Since we have created the ah
+		 * above which would have made use of the lids, now is
+		 * a good time to change them back to regular GIDs after
+		 * saving the extended LIDs.
+		 */
+		if (rdma_cap_opa_ah(priv->ca, priv->port) &&
+		    ib_is_opa_gid(&pathrec->sgid)) {
+			path->slid = opa_get_lid_from_gid(&pathrec->sgid);
+			pathrec->sgid = path->pathrec.sgid;
+		} else {
+			path->slid = pathrec->slid;
 		}
+
+		if (rdma_cap_opa_ah(priv->ca, priv->port) &&
+		    ib_is_opa_gid(&pathrec->dgid)) {
+			path->dlid = opa_get_lid_from_gid(&pathrec->dgid);
+			pathrec->dgid = path->pathrec.dgid;
+		} else {
+			path->dlid = pathrec->dlid;
+		}
+		ipoib_dbg(priv, "PathRec SGID %pI6 DGID %pI6\n",
+			  pathrec->sgid.raw, pathrec->dgid.raw);
+
 		path->pathrec = *pathrec;
 
 		old_ah   = path->ah;

drivers/infiniband/ulp/ipoib/ipoib.h
@@ -416,6 +416,8 @@ struct ipoib_path {
 
 	struct rb_node	      rb_node;
 	struct list_head      list;
+	u32		      dlid;
+	u32		      slid;
 	int  		      valid;
 };
 

drivers/infiniband/ulp/ipoib/ipoib_cm.c
@@ -38,6 +38,7 @@
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
 #include <linux/moduleparam.h>
+#include <rdma/ib_addr.h>
 
 #include "ipoib.h"
 
@@ -1340,6 +1341,19 @@ static void ipoib_cm_tx_start(struct work_struct *work)
 		neigh = p->neigh;
 		qpn = IPOIB_QPN(neigh->daddr);
 		memcpy(&pathrec, &p->path->pathrec, sizeof pathrec);
+
+		if (rdma_cap_opa_ah(priv->ca, priv->port)) {
+			if (p->path->dlid >= be16_to_cpu(IB_MULTICAST_LID_BASE))
+				pathrec.dgid.global.interface_id =
+					OPA_MAKE_GID(p->path->dlid);
+
+			if (p->path->slid >= be16_to_cpu(IB_MULTICAST_LID_BASE))
+				pathrec.sgid.global.interface_id =
+					OPA_MAKE_GID(p->path->slid);
+		}
+
+		ipoib_dbg(priv, "PathRec SGID %pI6 DGID %pI6\n",
+			  pathrec.sgid.raw, pathrec.dgid.raw);
 
 		spin_unlock_irqrestore(&priv->lock, flags);
 		netif_tx_unlock_bh(dev);

commit a38b86c54524ff853498b0703b6950332891571c

IB/IPoIB: Fix slid/dlid update from Path record response
slid and dlid are in big endian format and that needs to
be considered when saving them in ipoib slid/dlid local
data structures.

Fixes: 2c607e07a003("IB/IPoIB: More changes to support extended LIDs in
IPOIB")

Signed-off-by: Dasaratharaman Chandramouli <dasaratharaman.chandramouli@intel.com>

drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -730,7 +730,7 @@ static void path_rec_completion(int status,
 			path->slid = opa_get_lid_from_gid(&pathrec->sgid);
 			pathrec->sgid = path->pathrec.sgid;
 		} else {
-			path->slid = pathrec->slid;
+			path->slid = be16_to_cpu(pathrec->slid);
 		}
 
 		if (rdma_cap_opa_ah(priv->ca, priv->port) &&
@@ -738,7 +738,7 @@ static void path_rec_completion(int status,
 			path->dlid = opa_get_lid_from_gid(&pathrec->dgid);
 			pathrec->dgid = path->pathrec.dgid;
 		} else {
-			path->dlid = pathrec->dlid;
+			path->dlid = be16_to_cpu(pathrec->dlid);
 		}
 		ipoib_dbg(priv, "PathRec SGID %pI6 DGID %pI6\n",
 			  pathrec->sgid.raw, pathrec->dgid.raw);