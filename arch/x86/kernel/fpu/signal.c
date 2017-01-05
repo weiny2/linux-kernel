073c63e23713c992a92beb90660a44f12b2cf03c
Yu-cheng Yu <yu-cheng.yu@intel.com>
2017-01-05 13:48:31 -0800

    x86/cet/shstk: Handle signals for Shadow Stack
    To deliver a signal, create a Shadow Stack (SHSTK) restore token and put
    the token and the signal restorer address on the SHSTK.  For sigreturn,
    verify the token and restore the SHSTK pointer.
    
    Introduce a signal context extension struct 'sc_ext', which is used to save
    SHSTK restore token address and WAIT_ENDBR status.  WAIT_ENDBR will be
    introduced later in the Indirect Branch Tracking (IBT) series, but add that
    into sc_ext now to keep the struct stable in case the IBT series is applied
    later.
    
    v9:
    - Update CET MSR access according to XSAVES supervisor state changes.
    - Add 'wait_endbr' to struct 'sc_ext'.
    - Update and simplify signal frame allocation, setup, and restoration.
    - Update commit log text.
    
    v2:
    - Move CET status from sigcontext to a separate struct sc_ext, which is
      located above the fpstate on the signal frame.
    - Add a restore token for sigreturn address.
    
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>

--- arch/x86/kernel/fpu/signal.c
+++ arch/x86/kernel/fpu/signal.c
@@ -52,6 +52,69 @@ static inline int check_for_xstate(struct fxregs_state __user *buf,
 	return 0;
 }
 
+int save_cet_to_sigframe(void __user *fp, unsigned long restorer, int is_ia32)
+{
+	int err = 0;
+
+#ifdef CONFIG_X86_INTEL_CET
+	if (!current->thread.cet.shstk_enabled)
+		return 0;
+
+	if (fp) {
+		struct sc_ext ext = {0, 0, 0};
+
+		err = cet_setup_signal(is_ia32, restorer, &ext);
+		if (!err) {
+			void __user *p = fp;
+
+			ext.total_size = sizeof(ext);
+
+			if (is_ia32)
+				p += sizeof(struct fregs_state);
+
+			p += fpu_user_xstate_size + FP_XSTATE_MAGIC2_SIZE;
+			p = (void __user *)ALIGN((unsigned long)p, 8);
+
+			if (copy_to_user(p, &ext, sizeof(ext)))
+				return -EFAULT;
+		}
+	}
+#endif
+
+	return err;
+}
+
+static int restore_cet_from_sigframe(int is_ia32, void __user *fp)
+{
+	int err = 0;
+
+#ifdef CONFIG_X86_INTEL_CET
+	if (!current->thread.cet.shstk_enabled)
+		return 0;
+
+	if (fp) {
+		struct sc_ext ext = {0, 0, 0};
+		void __user *p = fp;
+
+		if (is_ia32)
+			p += sizeof(struct fregs_state);
+
+		p += fpu_user_xstate_size + FP_XSTATE_MAGIC2_SIZE;
+		p = (void __user *)ALIGN((unsigned long)p, 8);
+
+		if (copy_from_user(&ext, p, sizeof(ext)))
+			return -EFAULT;
+
+		if (ext.total_size != sizeof(ext))
+			return -EFAULT;
+
+		err = cet_restore_signal(is_ia32, &ext);
+	}
+#endif
+
+	return err;
+}
+
 /*
  * Signal frame handlers.
  */
@@ -367,6 +430,10 @@ static int __fpu__restore_sig(void __user *buf, void __user *buf_fx, int size)
 		pagefault_disable();
 		ret = copy_user_to_fpregs_zeroing(buf_fx, xfeatures_user, fx_only);
 		pagefault_enable();
+
+		if (!ret)
+			ret = restore_cet_from_sigframe(0, buf);
+
 		if (!ret) {
 			if (xfeatures_mask_supervisor())
 				copy_kernel_to_xregs(&fpu->state.xsave,
@@ -396,6 +463,10 @@ static int __fpu__restore_sig(void __user *buf, void __user *buf_fx, int size)
 
 		sanitize_restored_user_xstate(&fpu->state, envp, xfeatures_user,
 					      fx_only);
+
+		ret = restore_cet_from_sigframe((int)ia32_fxstate, buf);
+		if (ret)
+			goto err_out;
 
 		fpregs_lock();
 		if (unlikely(init_bv))
@@ -468,11 +539,31 @@ int fpu__restore_sig(void __user *buf, int ia32_frame)
 	return __fpu__restore_sig(buf, buf_fx, size);
 }
 
+static unsigned long fpu__alloc_sigcontext_ext(unsigned long sp)
+{
+	/*
+	 * sigcontext_ext is at: fpu + fpu_user_xstate_size +
+	 * FP_XSTATE_MAGIC2_SIZE, then aligned to 8.
+	 */
+#ifdef CONFIG_X86_INTEL_CET
+	if (cpu_x86_cet_enabled()) {
+		struct cet_status *cet = &current->thread.cet;
+
+		if (cet->shstk_enabled)
+			sp -= (sizeof(struct sc_ext) + 8);
+	}
+#endif
+
+	return sp;
+}
+
 unsigned long
 fpu__alloc_mathframe(unsigned long sp, int ia32_frame,
 		     unsigned long *buf_fx, unsigned long *size)
 {
 	unsigned long frame_size = xstate_sigframe_size();
+
+	sp = fpu__alloc_sigcontext_ext(sp);
 
 	*buf_fx = sp = round_down(sp - frame_size, 64);
 	if (ia32_frame && use_fxsr()) {
