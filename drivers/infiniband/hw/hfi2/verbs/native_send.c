1be81b8cebc7b0c7543160aa7596e776cc0c4749
Matt <matthew.brost@intel.com>
2018-06-18 13:37:19 -0700

    IB/hfi2: Fix bug in native verbs pid exchange
    Fix bug in native verbs pid exchange. The condition to
    check if the pid exchange has completed is incorrect which
    allows commands to be sent before the remote QP is
    configured to receive.
    
    INTERNAL: STL-38089
    
    Reviewed-by: Brian Welty <brian.welty@intel.com>
    Reviewed-by: Venkata Sandeep Dhanalakota <venkata.s.dhanalakota@intel.com>
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/infiniband/hw/hfi2/verbs/native_send.c
+++ drivers/infiniband/hw/hfi2/verbs/native_send.c
@@ -852,7 +852,7 @@ static inline bool native_send_ok(struct rvt_qp *qp)
 	if (unlikely(!(ib_rvt_state_ops[qp->state] & RVT_POST_SEND_OK)))
 		return false;
 	/* TODO - may be atomic, remove below with STL-37655 */
-	if (!qp_priv->tpid && qp->ibqp.qp_type != IB_QPT_UD)
+	if (qp->ibqp.qp_type != IB_QPT_UD && qp_priv->state != QP_STATE_RTS)
 		wait_for_completion_interruptible(&qp_priv->pid_xchg_comp);
 	return true;
 }
