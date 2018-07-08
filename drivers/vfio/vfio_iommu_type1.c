9259c0f8f7c4819ca39512cd47989ea9c09432af
John Hubbard <jhubbard@nvidia.com>
2018-07-08 13:24:49 -0700

    vfio/mm: convert to the new put_user_page() call
    For code that retains pages via get_user_pages*(),
    release those pages via the new put_user_page(),
    instead of put_page().
    
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: kvm@vger.kernel.org

--- drivers/vfio/vfio_iommu_type1.c
+++ drivers/vfio/vfio_iommu_type1.c
@@ -338,7 +338,7 @@ static int put_pfn(unsigned long pfn, int prot)
 		struct page *page = pfn_to_page(pfn);
 		if (prot & IOMMU_WRITE)
 			SetPageDirty(page);
-		put_page(page);
+		put_user_page(page);
 		return 1;
 	}
 	return 0;
@@ -372,7 +372,7 @@ static int vaddr_get_pfn(struct mm_struct *mm, unsigned long vaddr,
 		 */
 		if (ret > 0 && vma_is_fsdax(vmas[0])) {
 			ret = -EOPNOTSUPP;
-			put_page(page[0]);
+			put_user_page(page[0]);
 		}
 	}
 	up_read(&mm->mmap_sem);
