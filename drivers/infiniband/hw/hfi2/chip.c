40e6bf8e43ba96e330b71c762bfd81c0a60a1515
Welty, Brian <brian.welty@intel.com>
2018-07-26 16:46:32 -0700

    IB/hfi2: Disable reporting HDR link speed until user-space has support
    OpenMPI in our IFS releases will error out due to unknown link speed.
    Newer OpenMPI builds have this but are not yet available internally.
    Disable reporting HDR speeds until new OpenMPI is widely available to
    hfi2 users.
    
    Reviewed-by: Neel Desai <neel.desai@intel.com>
    Signed-off-by: Brian Welty <brian.welty@intel.com>

--- drivers/infiniband/hw/hfi2/chip.c
+++ drivers/infiniband/hw/hfi2/chip.c
@@ -3769,8 +3769,8 @@ static int hfi_pport_init(struct hfi_devdata *dd)
 		ppd->link_width_downgrade_enabled =
 					ppd->link_width_downgrade_supported;
 
-		ppd->link_speed_supported = OPA_LINK_SPEED_50G |
-					    OPA_LINK_SPEED_25G;
+		/* TODO - add OPA_LINK_SPEED_50G after new OMPI is widely available */
+		ppd->link_speed_supported = OPA_LINK_SPEED_25G;
 		ppd->link_speed_enabled = ppd->link_speed_supported;
 		/* give a reasonable active value, will be set on link up */
 		ppd->link_speed_active = OPA_LINK_SPEED_25G;
