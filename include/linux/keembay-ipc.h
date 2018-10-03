be7e286460170f7cc1fffdc6453a23e9114182da
Alessandrelli, Daniele <daniele.alessandrelli@intel.com>
2018-10-03 09:43:04 +0000

    keembay-ipc: Add Keem Bay IPC module
    Change-Id: Ib9ae2e1e604dc9a9453b50ad780454cf2c51f742
    Signed-off-by: Alessandrelli, Daniele <daniele.alessandrelli@intel.com>
    Signed-off-by: Murphy, Paul J <paul.j.murphy@intel.com>
    Signed-off-by: Jean-Hugues Decker <jean-hugues.decker@intel.com>
    
    (cherry picked from commit f2854501c4bc72c93fbc729dc52de4b220d882d2)

---
+++ include/linux/keembay-ipc.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * keembay-ipc.h - KeemBay IPC Linux Kernel API
+ *
+ * Copyright (C) 2018-2019 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef __KEEMBAY_IPC_H
+#define __KEEMBAY_IPC_H
+
+/* The possible node IDs. */
+enum {
+	KMB_IPC_NODE_ARM_CSS = 0,
+	KMB_IPC_NODE_LEON_MSS,
+};
+
+int intel_keembay_ipc_open_channel(u8 node_id, u16 chan_id);
+int intel_keembay_ipc_close_channel(u8 node_id, u16 chan_id);
+int intel_keembay_ipc_send(u8 node_id, u16 chan_id, uint32_t paddr,
+			   size_t size);
+int intel_keembay_ipc_recv(u8 node_id, u16 chan_id, uint32_t *paddr,
+			   size_t *size, u32 timeout);
+
+#endif /* __KEEMBAY_IPC_H */
