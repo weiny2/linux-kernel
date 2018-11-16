905a0fcbe7db6329d74d5370b5e4c59bcf7c1ce4
Yu-cheng Yu <yu-cheng.yu@intel.com>
2018-11-16 13:22:31 -0800

    x86: Introduce X86_FEATURE_RAR
    Introduce X86_FEATURE_RAR and enumeration of the feature.  The enumeration
    process finds X86_FEATURE_CORE_CAPABILITIES first, and then from MSR_IA32_
    CORE_CAPABILITIES, bit[1], finds the existence of X86_FEATURE_RAR.
    
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>

--- arch/x86/include/asm/disabled-features.h
+++ arch/x86/include/asm/disabled-features.h
@@ -56,6 +56,12 @@
 # define DISABLE_PTI		(1 << (X86_FEATURE_PTI & 31))
 #endif
 
+#ifdef CONFIG_X86_INTEL_RAR
+# define DISABLE_RAR		0
+#else
+# define DISABLE_RAR		(1 << (X86_FEATURE_RAR & 31))
+#endif
+
 /*
  * Make sure to add features to the correct mask
  */
@@ -70,7 +76,7 @@
 #define DISABLED_MASK8	0
 #define DISABLED_MASK9	(DISABLE_SMAP)
 #define DISABLED_MASK10	0
-#define DISABLED_MASK11	0
+#define DISABLED_MASK11	(DISABLE_RAR)
 #define DISABLED_MASK12	0
 #define DISABLED_MASK13	0
 #define DISABLED_MASK14	0
