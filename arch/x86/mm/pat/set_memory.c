47c7ad5a220eb59a8c41333e94c94db9d7447b87
Yu-cheng Yu <yu-cheng.yu@intel.com>
2019-03-15 10:10:21 -0700

    x86/mm: Change cpa_flush() to call flush_kernel_range() directly
    The function cpa_flush() calls __flush_tlb_one_kernel() and
    flush_tlb_all(), which is equivalent to flush_tlb_kernel_range().
    In preparation of RAR, replace them with just flush_tlb_kernel_range().
    
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>

--- arch/x86/mm/pat/set_memory.c
+++ arch/x86/mm/pat/set_memory.c
@@ -334,19 +334,11 @@ static void cpa_flush_all(unsigned long cache)
 	on_each_cpu(__cpa_flush_all, (void *) cache, 1);
 }
 
-static void __cpa_flush_tlb(void *data)
-{
-	struct cpa_data *cpa = data;
-	unsigned int i;
-
-	for (i = 0; i < cpa->numpages; i++)
-		__flush_tlb_one_kernel(fix_addr(__cpa_addr(cpa, i)));
-}
-
 static void cpa_flush(struct cpa_data *data, int cache)
 {
 	struct cpa_data *cpa = data;
 	unsigned int i;
+	unsigned long start, end;
 
 	BUG_ON(irqs_disabled() && !early_boot_irqs_disabled);
 
@@ -355,10 +347,9 @@ static void cpa_flush(struct cpa_data *data, int cache)
 		return;
 	}
 
-	if (cpa->numpages <= tlb_single_page_flush_ceiling)
-		on_each_cpu(__cpa_flush_tlb, cpa, 1);
-	else
-		flush_tlb_all();
+	start = fix_addr(__cpa_addr(cpa, 0));
+	end = fix_addr(__cpa_addr(cpa, cpa->numpages));
+	flush_tlb_kernel_range(start, end);
 
 	if (!cache)
 		return;
