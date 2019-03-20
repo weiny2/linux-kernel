754ec4e104b24c42e2ed618182bd71f1bd215552
Kyle Pelton <kyle.d.pelton@linux.intel.com>
2019-03-20 16:25:24 -0700

    video/logo:Add intel next logo
    Add new ppm logo and new config option to support intel-next
    branding
    
    logo is a 160x160 ppm 224 color file
    
    Signed-off-by: Kyle Pelton <kyle.d.pelton@linux.intel.com>

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -100,6 +100,10 @@ const struct linux_logo * __ref fb_find_logo(int depth)
 		/* SuperH Linux logo */
 		logo = &logo_superh_clut224;
 #endif
+#ifdef CONFIG_LOGO_LINUX_NEXT_CLUT224
+		/* Generic Linux logo */
+		logo = &logo_linux_next_clut224;
+#endif
 	}
 	return logo;
 }
