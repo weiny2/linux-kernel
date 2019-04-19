d40655ba95256d7890a6f6b42f253b7c98584a8e
Andrew Morton <akpm@linux-foundation.org>
2019-04-19 22:10:21 +0000

    linux-next-git-rejects
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -1315,10 +1315,7 @@ static int fastrpc_cb_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	int i, sessions = 0;
 	int rc;
-<<<<<<< HEAD
-=======
 	unsigned long flags;
->>>>>>> linux-next/akpm-base
 
 	cctx = dev_get_drvdata(dev->parent);
 	if (!cctx)
@@ -1347,11 +1344,7 @@ static int fastrpc_cb_probe(struct platform_device *pdev)
 		}
 	}
 	cctx->sesscount++;
-<<<<<<< HEAD
-	spin_unlock(&cctx->lock);
-=======
 	spin_unlock_irqrestore(&cctx->lock, flags);
->>>>>>> linux-next/akpm-base
 	rc = dma_set_mask(dev, DMA_BIT_MASK(32));
 	if (rc) {
 		dev_err(dev, "32-bit DMA enable failed\n");
