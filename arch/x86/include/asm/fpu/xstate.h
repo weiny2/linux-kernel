495214c48c6e84115cb61db4912677cf1da452d1
Chang S. Bae <chang.seok.bae@intel.com>
2019-06-19 09:12:31 -0700

    x86/fpu: Enable the AMX feature
    The kernel now supports and enables the AMX when a CPU has that
    capability. This change effectively brings to write XCR0 when a CPU has
    the feature with the first-use detection support.
    
    Also, define XTILE_DATA as a huge state, which means the kernel saves the
    states in an expanded xstate area.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: H. Peter Anvin <h.peter.anvin@intel.com>
    Cc: Tim Chen <tim.c.chen@intel.com>
    Cc: Andi Kleen <andi.kleen@intel.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Li Aubrey <aubrey.li@intel.com>
    Cc: Jing Liu <jing2.liu@intel.com>
    Cc: Ravi Shankar <ravi.v.shankar@intel.com>

--- arch/x86/include/asm/fpu/xstate.h
+++ arch/x86/include/asm/fpu/xstate.h
@@ -31,7 +31,8 @@
 				      XFEATURE_MASK_Hi16_ZMM	 | \
 				      XFEATURE_MASK_PKRU | \
 				      XFEATURE_MASK_BNDREGS | \
-				      XFEATURE_MASK_BNDCSR)
+				      XFEATURE_MASK_BNDCSR | \
+				      XFEATURE_MASK_XTILE)
 
 /* All currently supported supervisor features */
 #define XFEATURE_MASK_SUPERVISOR_SUPPORTED (XFEATURE_MASK_CET_USER | \
