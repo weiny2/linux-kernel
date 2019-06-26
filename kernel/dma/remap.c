5a3ebbb311c28f86116e8089df63a090725f9421
Andrew Morton <akpm@linux-foundation.org>
2019-06-26 00:09:36 +0000

    linux-next-rejects
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/dma/remap.c
+++ kernel/dma/remap.c
@@ -158,6 +158,9 @@ out:
 
 bool dma_in_atomic_pool(void *start, size_t size)
 {
+	if (unlikely(!atomic_pool))
+	return false;
+
 	return gen_pool_has_addr(atomic_pool, (unsigned long)start, size);
 }
 
