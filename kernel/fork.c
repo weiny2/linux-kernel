634f2909e7ef1ba94c5930f707b3e8e5b8888835
Andrew Morton <akpm@linux-foundation.org>
2019-06-26 00:09:44 +0000

    kernel/fork.c: export kernel_thread() to modules
    mutex-subsystem-synchro-test-module.patch needs this
    
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2451,6 +2451,7 @@ pid_t kernel_thread(int (*fn)(void *), void *arg, unsigned long flags)
 
 	return _do_fork(&args);
 }
+EXPORT_SYMBOL(kernel_thread);
 
 #ifdef __ARCH_WANT_SYS_FORK
 SYSCALL_DEFINE0(fork)
