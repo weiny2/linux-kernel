b343d8b7970e5151cd535a6c25a22eb86815eff5
Chang S. Bae <chang.seok.bae@intel.com>
2019-07-01 07:10:47 -0700

    x86/fpu/xstate: Support size calculation for expanded area
    For context switching, the kernel saves the FPU states in the pre-
    allocated area as part of task data for efficiency and other historical
    reasons. However, this FPU storage model is not as sustainable as the
    state size trends to grow. Some future extended features have huge size
    states to take a few pages, which makes it hard to be part of each task's
    FPU data. Also, the pre-allocation for all tasks is a waste of space as
    not all tasks need such a huge state. One approach, considered as to be
    simple and straightforward, is to keep those huge states in a separate
    area, and that allocated only when needed. We can keep the current
    storage model for those non-huge states.
    
    With the introduced subsets of the feature mask, the next infrastructural
    change is to support the size calculation for the expanded area and the
    default area. The calculation code needs some changes to support multiple
    sizes in two ways. In the first phase of the calculation, it directly
    reads CPUID values at most, and then during the second phase, iteratively
    checks each feature size and sums them to cross-check.
    
    Introduce a couple of helper functions, for the first phase, to perform
    mask-based size reads. Each helper supports standard or compact format.
    Also, rename the old relevant helper to avoid confusion.
    
    During the second phase, make the iterator figure out which are for the
    calculation.
    
    Subsequently, introduce a new global variable to keep the calculated size
    for the expanded area.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: H. Peter Anvin <h.peter.anvin@intel.com>
    Cc: Tim Chen <tim.c.chen@intel.com>
    Cc: Andi Kleen <andi.kleen@intel.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Li Aubrey <aubrey.li@intel.com>
    Cc: Jing2 Liu <jing2.liu@intel.com>
    Cc: Ravi Shankar <ravi.v.shankar@intel.com>

--- arch/x86/include/asm/fpu/xstate.h
+++ arch/x86/include/asm/fpu/xstate.h
@@ -20,6 +20,7 @@
 
 #define XSAVE_YMM_SIZE	    256
 #define XSAVE_YMM_OFFSET    (XSAVE_HDR_SIZE + XSAVE_HDR_OFFSET)
+#define XSAVE_FIRST_EXT_OFFSET XSAVE_YMM_OFFSET
 
 /* All currently supported user features */
 #define XFEATURE_MASK_USER_SUPPORTED (XFEATURE_MASK_FP | \
