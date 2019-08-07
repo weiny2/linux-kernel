7e02d79187bf6ad307c0fa779804bbc9061be7d2
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-08-07 18:17:09 +0300

    TO BE FOLDED: pinctrl: emmitsburg: Update offsets (fc-ebg-main-19ww25e)
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/pinctrl/intel/pinctrl-emmitsburg.c
+++ drivers/pinctrl/intel/pinctrl-emmitsburg.c
@@ -14,11 +14,11 @@
 
 #include "pinctrl-intel.h"
 
-#define EBG_PAD_OWN	0x020
-#define EBG_PADCFGLOCK	0x080
-#define EBG_HOSTSW_OWN	0x0b0
+#define EBG_PAD_OWN	0x0a0
+#define EBG_PADCFGLOCK	0x100
+#define EBG_HOSTSW_OWN	0x130
 #define EBG_GPI_IS	0x100
-#define EBG_GPI_IE	0x120
+#define EBG_GPI_IE	0x210
 
 #define EBG_GPP(r, s, e)				\
 	{						\
