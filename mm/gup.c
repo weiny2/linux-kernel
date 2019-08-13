accc475b86f1cb1f33d8c06f69523aef08f91444
John Hubbard <jhubbard@nvidia.com>
2019-08-13 16:06:46 -0700

    mm/gup: introduce FOLL_PIN flag for get_user_pages()
    FOLL_PIN is set by vaddr_pin_pages(). This is different than
    FOLL_LONGTERM, because even short term page pins need a new kind
    of tracking, if those pinned pages' data is going to potentially
    be modified.
    
    This situation is described in more detail in commit fc1d8e7cca2d
    ("mm: introduce put_user_page*(), placeholder versions").
    
    FOLL_PIN is added now, rather than waiting until there is code that
    takes action based on FOLL_PIN. That's because having FOLL_PIN in
    the code helps to highlight the differences between:
    
        a) get_user_pages(): soon to be deprecated. Used to pin pages,
           but without awareness of file systems that might use those
           pages,
    
        b) The original vaddr_pin_pages(): intended only for
           FOLL_LONGTERM and DAX use cases. This assumes direct IO
           and therefore is not applicable the most of the other
           callers of get_user_pages(), and
    
        c) The new vaddr_pin_pages(), which provides the correct
           get_user_pages() flags for all cases, by setting FOLL_PIN.
    
    Cc: Jan Kara <jack@suse.cz>
    Cc: Michal Hocko <mhocko@kernel.org>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>

--- mm/gup.c
+++ mm/gup.c
@@ -2484,6 +2484,9 @@ EXPORT_SYMBOL_GPL(get_user_pages_fast);
  * being made against.  Usually "current->mm".
  *
  * Expects mmap_sem to be read locked.
+ *
+ * Implementation note: this sets FOLL_PIN, which means that the pages must
+ * ultimately be released by put_user_page().
  */
 long vaddr_pin_pages(unsigned long addr, unsigned long nr_pages,
 		     unsigned int gup_flags, struct page **pages,
@@ -2491,7 +2494,7 @@ long vaddr_pin_pages(unsigned long addr, unsigned long nr_pages,
 {
 	long ret;
 
-	gup_flags |= FOLL_LONGTERM;
+	gup_flags |= FOLL_LONGTERM | FOLL_PIN;
 
 	if (!vaddr_pin || (!vaddr_pin->mm && !vaddr_pin->f_owner))
 		return -EINVAL;
