330e0a8d6ec9be477b7f3a1b24e89ae3df4aaba4
Tomas Winkler <tomas.winkler@intel.com>
2019-08-14 01:10:48 +0300

    mei: me: add eagle stream workstation DID
    Change-Id: I4907b6b4c0a1eecac2cd7019a4a519eeef25e0b8
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- drivers/misc/mei/hw-me-regs.h
+++ drivers/misc/mei/hw-me-regs.h
@@ -108,6 +108,8 @@
 #define MEI_DEV_ID_MCC        0x4B70  /* Mule Creek Canyon (EHL) */
 #define MEI_DEV_ID_MCC_4      0x4B75  /* Mule Creek Canyon 4 (EHL) */
 
+#define MEI_DEV_ID_EGLS       0x1BE0  /* Emmitsburg (Eagle Stream) WS */
+
 /*
  * MEI HW Section
  */
