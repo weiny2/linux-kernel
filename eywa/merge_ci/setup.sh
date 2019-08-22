882327265ad9a135adacc8f05cd12a423cfe03fe
Intel Next Project <intel-next-maintainers@eclists.intel.com>
2019-08-22 12:35:21 -0700

    setup:sh: Add changes required for new system
    copy the merge.py and manifest_in to root dir.
    delete legacy scripts

--- eywa/merge_ci/setup.sh
+++ eywa/merge_ci/setup.sh
@@ -9,14 +9,12 @@ git checkout eywa -f
 git reset --hard origin/eywa
 rm -rfv .git/rr-cache
 mkdir .git/rr-cache
-cp -rf eywa/merge_ci/blacklist .
-cp -rf eywa/merge_ci/blacklist.py .
-cp -rf eywa/merge_ci/replace .
-cp -rf eywa/merge_ci/replace.py .
 cp -rf eywa/merge_ci/step1.sh .
 cp -rf eywa/merge_ci/step2.sh .
 cp -rf eywa/merge_ci/step3.sh .
 cp -rf eywa/merge_ci/merge.sh .
+cp -rf eywa/merge_ci/merge.py .
+cp -rf eywa/manifest_in.json .
 mkdir -p eywa/merge_ci/rr-cache/
 if [ -d eywa/merge_ci/rr-cache/ ]; then
 	#check if there are files inside folder and copy
