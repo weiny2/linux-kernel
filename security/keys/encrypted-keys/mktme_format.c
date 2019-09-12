9a0851647438a77af578a5de8dcf06da4f75a8a0
Alison Schofield <alison.schofield@intel.com>
2019-09-12 20:17:46 +0800

    encrypted/mktme: Move an MKTME payload into a cache aligned struct
    In preparation for programming the key into the hardware, move
    the key payload into a cache aligned structure. This alignment
    is a requirement of the MKTME hardware.
    
    Use the slab allocator to have this structure readily available.
    
    Signed-off-by: Alison Schofield <alison.schofield@intel.com>

--- security/keys/encrypted-keys/mktme_format.c
+++ security/keys/encrypted-keys/mktme_format.c
@@ -6,12 +6,15 @@
 #include <linux/key.h>
 #include <linux/mm.h>
 #include <linux/random.h>
+#include <linux/slab.h>
 #include <asm/intel_pconfig.h>
 #include <asm/mktme.h>
 
 #include "mktme_format.h"
 
 static DEFINE_SPINLOCK(mktme_lock);
+static unsigned int mktme_available_keyids;  /* Free Hardware KeyIDs */
+static struct kmem_cache *mktme_prog_cache;  /* Hardware programming cache */
 
 /*
  * Maximum value of mktme_available_keyids is the total number
@@ -83,17 +86,46 @@ void mktme_remove_key(struct key *key)
 	spin_unlock_irqrestore(&mktme_lock, flags);
 }
 
+/* Copy the payload to the HW programming structure and program this KeyID */
+static int mktme_program_keyid(int keyid, struct mktme_payload *payload)
+{
+	struct mktme_key_program *kprog = NULL;
+	int ret;
+
+	kprog = kmem_cache_zalloc(mktme_prog_cache, GFP_KERNEL);
+	if (!kprog)
+		return -ENOMEM;
+
+	/* Hardware programming requires cached aligned struct */
+	kprog->keyid = keyid;
+	kprog->keyid_ctrl = payload->keyid_ctrl;
+	memcpy(kprog->key_field_1, payload->data_key, MKTME_AES_XTS_SIZE);
+	memcpy(kprog->key_field_2, payload->tweak_key, MKTME_AES_XTS_SIZE);
+
+	ret = MKTME_PROG_SUCCESS;	/* Future programming call */
+	kmem_cache_free(mktme_prog_cache, kprog);
+	return ret;
+}
+
 int mktme_get_key(struct key *key, struct mktme_payload *payload)
 {
 	unsigned long flags;
-	int keyid;
+	int keyid, ret;
 
 	spin_lock_irqsave(&mktme_lock, flags);
 	keyid = mktme_reserve_keyid(key);
 	spin_unlock_irqrestore(&mktme_lock, flags);
 	if (!keyid)
 		return -ENOKEY;
-	return 0;
+
+	ret = mktme_program_keyid(keyid, payload);
+	if (ret == MKTME_PROG_SUCCESS)
+		return ret;
+
+	spin_lock_irqsave(&mktme_lock, flags);
+	mktme_release_keyid(keyid);
+	spin_unlock_irqrestore(&mktme_lock, flags);
+	return -ENOKEY;
 }
 
 static void mktme_build_new_payload(struct mktme_payload *payload)
@@ -153,9 +185,19 @@ static int __init init_mktme(void)
 	if (!mktme_map)
 		return -ENOMEM;
 
+	/* Used to program the hardware key tables */
+	mktme_prog_cache = KMEM_CACHE(mktme_key_program, SLAB_PANIC);
+	if (!mktme_prog_cache)
+		goto free_map;
+
 	pr_notice("Key type encrypted:mktme initialized\n");
 	mktme_keytype_enabled = true;
 	return 0;
+
+free_map:
+	kvfree(mktme_map);
+	pr_warn("Key type encrypted:mktme initialization failed\n");
+	return -ENOMEM;
 }
 
 late_initcall(init_mktme);
