ec2b3065da5c5136716ac7a8921be9fcd149f757
Fenghua Yu <fenghua.yu@intel.com>
2019-09-22 23:19:04 +0000

    x86/pks: Enable PKS
    Define ARCH_HAS_SUPERVISOR_PKEYS for Intel CPUs.  Then set CR4.PKS[24]
    bit to enable Protection Key for Supervisor pages (PKS) on the CPU when
    Supervisor PKEYS are supported by the CPU instance.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes from Ira
    	Add in ARCH_HAS_SUPERVISOR_PKEYS
    	Move function into CONFIG_X86_INTEL_MEMORY_PROTECTION_KEYS
    	Remove unnecessary comment in setup_pks()
    
    squash; prev pks patch

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -492,6 +492,18 @@ static __init int setup_disable_pku(char *arg)
 __setup("nopku", setup_disable_pku);
 #endif /* CONFIG_X86_64 */
 
+#ifdef CONFIG_ARCH_HAS_SUPERVISOR_PKEYS
+static __always_inline void setup_pks(struct cpuinfo_x86 *c)
+{
+	if (!cpu_feature_enabled(X86_FEATURE_PKS))
+		return;
+
+	cr4_set_bits(X86_CR4_PKS);
+}
+#else
+static __always_inline void setup_pks(struct cpuinfo_x86 *c) { }
+#endif /* CONFIG_ARCH_HAS_SUPERVISOR_PKEYS */
+
 /*
  * Some CPU features depend on higher CPUID levels, which may not always
  * be available due to CPUID level capping or broken virtualization
@@ -1521,6 +1533,7 @@ static void identify_cpu(struct cpuinfo_x86 *c)
 
 	x86_init_rdrand(c);
 	setup_pku(c);
+	setup_pks(c);
 
 	/*
 	 * Clear/Set all flags overridden by options, need do it
