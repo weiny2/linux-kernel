8dfd8ce65f738375abba76a6fc28ea75049426b9
Kyle Pelton <kyle.d.pelton@intel.com>
2019-12-02 11:57:56 -0800

    regen_configs.sh:Use dcg fragments on dcg build
    Fix typo of where dcg_fragments are placed
    
    Signed-off-by: Kyle Pelton <kyle.d.pelton@intel.com>

--- eywa/merge_ci/regen_configs.sh
+++ eywa/merge_ci/regen_configs.sh
@@ -34,9 +34,9 @@ echo $RULE >>$LOG_FILE
 #intel-next ubuntu config
 create_defconfig intel_next_generic_defconfig intel_next_generic_defconfig  "intel_next_config_options.config" 
 #intel-next clear config base off ubuntu with few options set to y
-create_defconfig intel_next_generic_defconfig intel_next_clear_generic_defconfig  "intel_next_config_options.config dcg_fragments/dcg.*.config clear_fragments/clear.*.config" 
+create_defconfig intel_next_generic_defconfig intel_next_clear_generic_defconfig  "intel_next_config_options.config clear_fragments/clear.*.config"
 #intel-next dcg rpm config  with dbg off
-create_defconfig dcg_x86_64_defconfig  dcg_x86_64_defconfig "intel_next_config_options.config clear_fragments/clear.*.config"  
+create_defconfig dcg_x86_64_defconfig  dcg_x86_64_defconfig "intel_next_config_options.config dcg_fragments/dcg.*.config clear_fragments/clear.*.config"
 #intel-next ubuntu based fedora rpm config with dbg off 
 create_defconfig intel_next_generic_defconfig intel_next_rpm_defconfig  "intel_next_config_options.config fedora_fragments/fedora.*.config clear_fragments/clear.*.config"
 
