724f564d46be626e07d774540439cef47454e79f
Intel Next Project <intel-next-maintainers@eclists.intel.com>
2019-12-11 14:01:34 -0800

    setup.sh:Fix incorrect remote for drm rr-cache
    Cleaned up script and fixed wrong remote name for drm
    
    Signed-off-by: Intel Next Project <intel-next-maintainers@eclists.intel.com>

--- eywa/merge_ci/setup.sh
+++ eywa/merge_ci/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Intel next setup script
+#This script is to be ran in the kernel root before ./merge.py is ran
 set -e
 git fetch
 git config rerere.enabled true
@@ -9,22 +11,35 @@ cp -rf eywa/merge_ci/regen_configs.sh .
 cp -rf eywa/merge_ci/merge.py .
 cp -rf eywa/merge_ci/step3.sh .
 cp -rf eywa/manifest_in.json .
-
-rm -rfv .git/rr-cache
+#Remove from old merge
+rm -rf patch-manifest
+rm -rf intel-next-merge*.log
+rm -rf .git/rr-cache
 mkdir .git/rr-cache
+
+#ADD freedesktop rr-cache to next rr cache
+#requested by rodrigo vivi
+echo "Adding drm rr-cache"
 set +e
-git remote add drm-cache git://anongit.freedesktop.org/drm/drm-tip
+git remote | grep -q drm-cache
+if [ $? != 0 ]; then
+
+	git remote add drm-cache git://anongit.freedesktop.org/drm/drm-tip
+fi
 set -e
 git fetch drm-cache
-git checkout drm_cache/origin/rerere-cache -- rr-cache 
+git checkout drm-cache/rerere-cache -- rr-cache 
+cp -rf rr-cache .git
+rm -rf rr-cache
 
+
+echo "Adding internal rr-cache"
+#Handle internal rr-cache
 mkdir -p eywa/merge_ci/rr-cache/
 if [ -d eywa/merge_ci/rr-cache/ ]; then
 	#check if there are files inside folder and copy
 	if [ "$(ls -A eywa/merge_ci/rr-cache)" ]; then
-		cp -rfv eywa/merge_ci/rr-cache/* .git/rr-cache
+		cp -rf eywa/merge_ci/rr-cache/* .git/rr-cache
 	fi
 fi
-cp -rfv rr-cache .git
-rm -rf rr-cache
 
