9ebfa73bcaa1166a14f68de1012dc227097df0be
Fenghua Yu <fenghua.yu@intel.com>
2020-01-28 18:05:14 +0000

    x86/pks: Context switch PKRS MSR per task
    The PKRS MSR is handled as a per task MSR so that each task can have
    different supervisor protection. The MSR can be changed in each task and
    needs to be context switched.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -28,6 +28,7 @@ struct vm86;
 #include <asm/vmxfeatures.h>
 #include <asm/vdso/processor.h>
 #include <asm/cet.h>
+#include <asm/pkeys_types.h>
 
 #include <linux/personality.h>
 #include <linux/cache.h>
@@ -547,6 +548,10 @@ struct thread_struct {
 #ifdef CONFIG_X86_INTEL_CET
 	struct cet_status	cet;
 #endif
+#ifdef	CONFIG_X86_INTEL_MEMORY_PROTECTION_KEYS
+	/* Protection key register for supervisor (lower 32 bits). */
+	u32			pkrs;
+#endif
 
 	/* Floating point and extended processor state */
 	struct fpu		fpu;
@@ -869,6 +874,7 @@ static inline void spin_lock_prefetch(const void *x)
 	.sp0			= TOP_OF_INIT_STACK,			  \
 	.sysenter_cs		= __KERNEL_CS,				  \
 	.addr_limit		= KERNEL_DS,				  \
+	INIT_THREAD_PKRS						  \
 }
 
 #define KSTK_ESP(task)		(task_pt_regs(task)->sp)
@@ -924,6 +930,7 @@ static inline void spin_lock_prefetch(const void *x)
 
 #define INIT_THREAD  {						\
 	.addr_limit		= KERNEL_DS,			\
+	INIT_THREAD_PKRS						\
 }
 
 extern unsigned long KSTK_ESP(struct task_struct *task);
