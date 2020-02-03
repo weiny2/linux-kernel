f42a1c312afce19885221df3a759ffa4c00a321b
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-02-03 15:31:27 +0200

    console: Drop misleading comment
    	/* find the last or real console */
    
    This comment is misleading. The purpose of the loop is to check
    if we are trying to register boot console after a real one has
    already been registered. This is already mentioned in a comment
    above.
    
    Link: http://lkml.kernel.org/r/20200203133130.11591-4-andriy.shevchenko@linux.intel.com
    To: linux-kernel@vger.kernel.org
    To: Steven Rostedt <rostedt@goodmis.org>
    Suggested-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    [pmladek@suse.com: Updated commit message.]
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2668,7 +2668,6 @@ void register_console(struct console *newcon)
 	 * already have a valid console
 	 */
 	if (newcon->flags & CON_BOOT) {
-		/* find the last or real console */
 		for_each_console(bcon) {
 			if (!(bcon->flags & CON_BOOT)) {
 				pr_info("Too late to register bootconsole %s%d\n",
