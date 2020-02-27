f11d646aef422d70f82c59bd0ec0bd3918dd2586
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-02-26 16:51:34 -0800

    PCI/AER: Allow clearing Error Status Register in FF mode
    As per PCI firmware specification r3.2 System Firmware Intermediary
    (SFI) _OSC and DPC Updates ECR
    (https://members.pcisig.com/wg/PCI-SIG/document/13563), sec titled "DPC
    Event Handling Implementation Note", page 10, Error Disconnect Recover
    (EDR) support allows OS to handle error recovery and clearing Error
    Registers even in FF mode. So create new API pci_aer_raw_clear_status()
    which allows clearing AER registers without FF mode checks.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -655,6 +655,7 @@ extern const struct attribute_group aer_stats_attr_group;
 void pci_aer_clear_fatal_status(struct pci_dev *dev);
 void pci_aer_clear_device_status(struct pci_dev *dev);
 int pci_cleanup_aer_error_status_regs(struct pci_dev *dev);
+int pci_aer_raw_clear_status(struct pci_dev *dev);
 #else
 static inline void pci_no_aer(void) { }
 static inline void pci_aer_init(struct pci_dev *d) { }
@@ -662,6 +663,10 @@ static inline void pci_aer_exit(struct pci_dev *d) { }
 static inline void pci_aer_clear_fatal_status(struct pci_dev *dev) { }
 static inline void pci_aer_clear_device_status(struct pci_dev *dev) { }
 static inline int pci_cleanup_aer_error_status_regs(struct pci_dev *dev)
+{
+	return -EINVAL;
+}
+static inline int pci_aer_raw_clear_status(struct pci_dev *dev)
 {
 	return -EINVAL;
 }
