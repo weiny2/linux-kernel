b1d609a4ac62784454640bf1a5857c3feaa5b628
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-02-26 17:24:45 -0800

    PCI/ERR: Return status of pcie_do_recovery()
    As per the Downstream Port Containment Related Enhancements ECN to the
    PCI Firmware Specification r3.2, sec 4.5.1, table 4-4, Support for Error
    Disconnect Recover (EDR) implies that the OS will invalidate the
    software state associated with child devices of the port without
    attempting to access the child device hardware. If the OS supports
    Downstream Port Containment (DPC), as indicated by the OS setting bit 7
    of _OSC control field, the OS shall attempt to recover the child devices
    if the port implements the Downstream Port Containment Extended
    Capability. If the OS continues operation, the OS must inform the
    Firmware of the status of the recovery operation via the _OST method.
    
    So in adding EDR support, to report status of error recovery via _OST,
    we need to know the status of error recovery. So add support to return
    the status of pcie_do_recovery() function.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- drivers/pci/pcie/err.c
+++ drivers/pci/pcie/err.c
@@ -186,9 +186,9 @@ static pci_ers_result_t reset_link(struct pci_dev *dev,
 	return status;
 }
 
-void pcie_do_recovery(struct pci_dev *dev,
-		      enum pci_channel_state state,
-		      pci_ers_result_t (*reset_cb)(struct pci_dev *pdev))
+pci_ers_result_t pcie_do_recovery(struct pci_dev *dev,
+			enum pci_channel_state state,
+			pci_ers_result_t (*reset_cb)(struct pci_dev *pdev))
 {
 	pci_ers_result_t status = PCI_ERS_RESULT_CAN_RECOVER;
 	struct pci_bus *bus;
@@ -240,11 +240,13 @@ void pcie_do_recovery(struct pci_dev *dev,
 	pci_aer_clear_device_status(dev);
 	pci_cleanup_aer_uncorrect_error_status(dev);
 	pci_info(dev, "device recovery successful\n");
-	return;
+	return status;
 
 failed:
 	pci_uevent_ers(dev, PCI_ERS_RESULT_DISCONNECT);
 
 	/* TODO: Should kernel panic here? */
 	pci_info(dev, "device recovery failed\n");
+
+	return status;
 }
