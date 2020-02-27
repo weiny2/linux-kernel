fcc05b113bf5b27f9003f9eb396f3ec725a36ed2
Ira Weiny <ira.weiny@intel.com>
2020-02-27 13:44:30 -0800

    x86/pks: Add PKS test code
    The core PKS functionality provides an interface for kernel users to
    reserve keys to their domains set up the page tables with those keys and
    control access to those domains when needed.
    
    Define test code which exercises the core functionality of PKS via a
    debugfs entry.  Basic checks can be triggered on boot with a kernel
    command line option while both basic and preemption checks can be
    triggered with separate debugfs values.
    
    debugfs controls are:
    
    '0' -- Run access tests with a single pkey
    '1' -- Set up the pkey register with no access for the pkey allocated to
           this fd
    '2' -- Check that the pkey register updated in '1' is still the same.
           (To be used after a forced context switch.)
    '3' -- Allocate all pkeys possible and run tests on each pkey allocated.
           DEFAULT when run at boot.
    
    Closing the fd will cleanup and release the pkey, therefore to fully
    exercise context switch testing a user space program is provided in:
    
    	.../tools/testing/selftests/x86/test_pks.c
    
    Co-developed-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes from RFV V2
    	Adjust for having the exception handling code ahead of this in
    	the series
    	Adjust for global update parameter
    	Rebased to 5.9-rc1

--- arch/x86/include/asm/pkeys.h
+++ arch/x86/include/asm/pkeys.h
@@ -173,4 +173,13 @@ static inline void pks_update_protection(int pkey, unsigned long protection,
 }
 #endif /* CONFIG_ARCH_HAS_SUPERVISOR_PKEYS */
 
+#if defined(CONFIG_PKS_TESTING)
+bool pks_test_armed_and_clear(u32 *val);
+#else
+static inline bool pks_test_armed_and_clear(u32 *val)
+{
+	return false;
+}
+#endif
+
 #endif /*_ASM_X86_PKEYS_H */
