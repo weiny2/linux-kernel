0c63a600494163995fb4d3d49c5302c5e48ba746
Kranthi Kuntala <kranthi.kuntala@intel.com>
2020-03-05 16:39:58 +0200

    thunderbolt: Add support for on-board retimers
    USB4 spec specifies standard access to retimers (both on-board and
    cable) through USB4 port sideband access. This makes it possible to
    upgrade their firmware in the same way than we already do with the
    routers.
    
    This enumerates on-board retimers under each USB4 port when the link
    comes up and adds them to the bus under the router the retimer belongs
    to. Retimers are exposed in sysfs with name like <device>:<port>.<index>
    where device is the router the retimer belongs to, port is the USB4 port
    the retimer is connected to and index is the retimer index under that
    port (starting from 1). This applies to the upstream USB4 port as well
    so if there is on-board retimer between the port and the router it is
    also added accordingly.
    
    At this time we do not add cable retimers but there is no techincal
    restriction to do so in the future if needed. It is not clear whether it
    makes sense to upgrade their firmwares and at least Thunderbolt 3 cables
    it has not been done outside of lab environments.
    
    The sysfs interface is made to follow the router NVM upgrade to make it
    easy to extend the existing userspace (fwupd) to handle these as well.
    
    Signed-off-by: Kranthi Kuntala <kranthi.kuntala@intel.com>
    Co-developed-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

--- drivers/thunderbolt/tb.c
+++ drivers/thunderbolt/tb.c
@@ -339,6 +339,9 @@ static void tb_scan_port(struct tb_port *port)
 		tb_port_dbg(port, "port already has a remote\n");
 		return;
 	}
+
+	tb_retimer_scan(port);
+
 	sw = tb_switch_alloc(port->sw->tb, &port->sw->dev,
 			     tb_downstream_route(port));
 	if (IS_ERR(sw)) {
@@ -394,6 +397,9 @@ static void tb_scan_port(struct tb_port *port)
 
 	if (tb_enable_tmu(sw))
 		tb_sw_warn(sw, "failed to enable TMU\n");
+
+	/* Scan upstream retimers */
+	tb_retimer_scan(upstream_port);
 
 	/*
 	 * Create USB 3.x tunnels only when the switch is plugged to the
@@ -474,6 +480,7 @@ static void tb_free_unplugged_children(struct tb_switch *sw)
 			continue;
 
 		if (port->remote->sw->is_unplugged) {
+			tb_retimer_remove_all(port);
 			tb_remove_dp_resources(port->remote->sw);
 			tb_switch_lane_bonding_disable(port->remote->sw);
 			tb_switch_remove(port->remote->sw);
@@ -869,6 +876,8 @@ static void tb_handle_hotplug(struct work_struct *work)
 		goto put_sw;
 	}
 	if (ev->unplug) {
+		tb_retimer_remove_all(port);
+
 		if (tb_port_has_remote(port)) {
 			tb_port_dbg(port, "switch unplugged\n");
 			tb_sw_set_unplugged(port->remote->sw);
@@ -1113,6 +1122,7 @@ static int tb_free_unplugged_xdomains(struct tb_switch *sw)
 		if (tb_is_upstream_port(port))
 			continue;
 		if (port->xdomain && port->xdomain->is_unplugged) {
+			tb_retimer_remove_all(port);
 			tb_xdomain_remove(port->xdomain);
 			port->xdomain = NULL;
 			ret++;
