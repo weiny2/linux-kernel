5afddc3ff7789478067ad69a813e0c1403bc74e7
Gage Eads <gage.eads@intel.com>
2020-03-08 14:34:52 -0500

    dlb: add device ioctl layer and first 4 ioctls
    This commit introduces the dlb device ioctl layer, and the first four
    ioctls: query device version, driver version, and available resources; and
    create a scheduling domain.
    
    The PF operation for scheduling domain creation will be added in a
    subsequent commit.
    
    Signed-off-by: Gage Eads <gage.eads@intel.com>

---
+++ drivers/misc/dlb/dlb_ioctl.c
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause)
+/* Copyright(c) 2017-2020 Intel Corporation */
+
+#include <uapi/linux/dlb_user.h>
+
+#include "dlb_ioctl.h"
+#include "dlb_main.h"
+
+/* [7:0]: device revision, [15:8]: device version */
+#define DLB_SET_DEVICE_VERSION(ver, rev) (((ver) << 8) | (rev))
+
+static int dlb_ioctl_get_device_version(struct dlb_dev *dev,
+					unsigned long user_arg)
+{
+	struct dlb_get_device_version_args arg;
+	struct dlb_cmd_response response;
+
+	dev_dbg(dev->dlb_device, "Entering %s()\n", __func__);
+
+	response.status = 0;
+	response.id = DLB_SET_DEVICE_VERSION(1, dev->revision);
+
+	if (copy_from_user(&arg, (void __user *)user_arg, sizeof(arg))) {
+		dev_err(dev->dlb_device,
+			"[%s()] Invalid ioctl argument pointer\n", __func__);
+		return -EFAULT;
+	}
+
+	if (copy_to_user((void __user *)arg.response,
+			 &response,
+			 sizeof(response))) {
+		dev_err(dev->dlb_device,
+			"[%s()] Invalid ioctl response pointer\n", __func__);
+		return -EFAULT;
+	}
+
+	dev_dbg(dev->dlb_device, "Exiting %s()\n", __func__);
+
+	return 0;
+}
+
+static int dlb_ioctl_create_sched_domain(struct dlb_dev *dev,
+					 unsigned long user_arg)
+{
+	struct dlb_create_sched_domain_args arg;
+	struct dlb_cmd_response response;
+	int ret;
+
+	response.status = 0;
+
+	dev_dbg(dev->dlb_device, "Entering %s()\n", __func__);
+
+	if (copy_from_user(&arg, (void __user *)user_arg, sizeof(arg))) {
+		dev_err(dev->dlb_device,
+			"[%s()] Invalid ioctl argument pointer\n", __func__);
+		return -EFAULT;
+	}
+
+	mutex_lock(&dev->resource_mutex);
+
+	ret = dev->ops->create_sched_domain(&dev->hw, &arg, &response);
+	if (ret)
+		goto unlock;
+
+unlock:
+	mutex_unlock(&dev->resource_mutex);
+
+	if (copy_to_user((void __user *)arg.response,
+			 &response,
+			 sizeof(response))) {
+		dev_err(dev->dlb_device,
+			"[%s()] Invalid ioctl response pointer\n", __func__);
+		return -EFAULT;
+	}
+
+	dev_dbg(dev->dlb_device, "Exiting %s()\n", __func__);
+
+	return ret;
+}
+
+static int dlb_ioctl_get_num_resources(struct dlb_dev *dev,
+				       unsigned long user_arg)
+{
+	struct dlb_get_num_resources_args arg;
+	int ret;
+
+	dev_dbg(dev->dlb_device, "Entering %s()\n", __func__);
+
+	mutex_lock(&dev->resource_mutex);
+
+	ret = dev->ops->get_num_resources(&dev->hw, &arg);
+
+	mutex_unlock(&dev->resource_mutex);
+
+	if (copy_to_user((void __user *)user_arg,
+			 &arg,
+			 sizeof(struct dlb_get_num_resources_args))) {
+		dev_err(dev->dlb_device, "Invalid DLB resources pointer\n");
+		return -EFAULT;
+	}
+
+	dev_dbg(dev->dlb_device, "Exiting %s()\n", __func__);
+
+	return ret;
+}
+
+static int dlb_ioctl_get_driver_version(struct dlb_dev *dev,
+					unsigned long user_arg)
+{
+	struct dlb_get_driver_version_args arg;
+	struct dlb_cmd_response response;
+
+	dev_dbg(dev->dlb_device, "Entering %s()\n", __func__);
+
+	response.status = 0;
+	response.id = DLB_VERSION;
+
+	if (copy_from_user(&arg, (void __user *)user_arg, sizeof(arg))) {
+		dev_err(dev->dlb_device,
+			"[%s()] Invalid ioctl argument pointer\n", __func__);
+		return -EFAULT;
+	}
+
+	if (copy_to_user((void __user *)arg.response,
+			 &response,
+			 sizeof(response))) {
+		dev_err(dev->dlb_device,
+			"[%s()] Invalid ioctl response pointer\n", __func__);
+		return -EFAULT;
+	}
+
+	dev_dbg(dev->dlb_device, "Exiting %s()\n", __func__);
+
+	return 0;
+}
+
+typedef int (*dlb_ioctl_callback_fn_t)(struct dlb_dev *dev, unsigned long arg);
+
+static dlb_ioctl_callback_fn_t dlb_ioctl_callback_fns[NUM_DLB_CMD] = {
+	dlb_ioctl_get_device_version,
+	dlb_ioctl_create_sched_domain,
+	dlb_ioctl_get_num_resources,
+	dlb_ioctl_get_driver_version,
+};
+
+int dlb_ioctl_dispatcher(struct dlb_dev *dev,
+			 unsigned int cmd,
+			 unsigned long arg)
+{
+	if (_IOC_NR(cmd) >= NUM_DLB_CMD) {
+		dev_err(dev->dlb_device, "[%s()] Unexpected DLB command %d\n",
+			__func__, _IOC_NR(cmd));
+		return -1;
+	}
+
+	return dlb_ioctl_callback_fns[_IOC_NR(cmd)](dev, arg);
+}
