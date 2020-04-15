f29f74cfc60e214e5825b48a0294aeb526acd45c
Gayatri Kammela <gayatri.kammela@intel.com>
2020-04-15 16:48:20 -0700

    cpufreq: intel_pstate: Add Rocket Lake support
    Signed-off-by: Gayatri Kammela <gayatri.kammela@intel.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -1936,6 +1936,8 @@ static const struct x86_cpu_id intel_pstate_cpu_ids[] = {
 	X86_MATCH(ATOM_GOLDMONT_PLUS,	core_funcs),
 	X86_MATCH(SKYLAKE_X,		core_funcs),
 	X86_MATCH(TIGERLAKE,		core_funcs),
+	X86_MATCH(ROCKETLAKE_L,		core_funcs),
+	X86_MATCH(ROCKETLAKE,		core_funcs),
 	{}
 };
 MODULE_DEVICE_TABLE(x86cpu, intel_pstate_cpu_ids);
