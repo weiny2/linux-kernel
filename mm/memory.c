bd4bfeec85688221725bfa3f69098fa23ed3447a
Yu-cheng Yu <yu-cheng.yu@intel.com>
2020-04-17 15:31:33 -0700

    mm: Fix do_numa_page

--- mm/memory.c
+++ mm/memory.c
@@ -4032,7 +4032,7 @@ static vm_fault_t do_numa_page(struct vm_fault *vmf)
 	pte = pte_modify(old_pte, vma->vm_page_prot);
 	pte = pte_mkyoung(pte);
 	if (was_writable)
-		pte = pte_mkwrite(pte);
+		pte = maybe_mkwrite(pte, vma);
 	ptep_modify_prot_commit(vma, vmf->address, vmf->pte, old_pte, pte);
 	update_mmu_cache(vma, vmf->address, vmf->pte);
 
