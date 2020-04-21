c080ef9c0e39ff62ccd750d07f3db844b52da9dc
Kyung Min Park <kyung.min.park@intel.com>
2020-04-20 17:30:52 -0700

    x86/cpufeatures: Enumerate TSX suspend load address tracking instruction
    Intel TSX suspend load tracking instructions aim to give a way to
    choose which memory accesses do not need to be tracked in the TSX
    read set. Add TSX suspend load tracking CPUID feature flag TSXLDTRK
    for enumeration.
    
    A processor supports Intel TSX suspend load address tracking if
    CPUID.0x07.0x0:EDX[16] is present. An application must check if the
    processor supports Intel TSX suspend load address tracking before it uses
    the Intel TSX suspend load tracking instructions (XSUSLDTRK, XRESLDTRK).
    These instructions will generate a #UD exception when used on a
    processor that does not support TSX suspend load tracking.
    
    The CPU feature flag is shown as "tsxldtrk" in /proc/cpuinfo.
    
    Detailed information on the instructions and CPUID feature flag TSXLDTRK
    can be found in the latest Intel Architecture Instruction Set Extensions
    and Future Features Programming Reference and Intel 64 and IA-32
    Architectures Software Developer's Manual.
    
    Signed-off-by: Kyung Min Park <kyung.min.park@intel.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -367,6 +367,7 @@
 #define X86_FEATURE_MD_CLEAR		(18*32+10) /* VERW clears CPU buffers */
 #define X86_FEATURE_TSX_FORCE_ABORT	(18*32+13) /* "" TSX_FORCE_ABORT */
 #define X86_FEATURE_SERIALIZE		(18*32+14) /* SERIALIZE instruction */
+#define X86_FEATURE_TSXLDTRK		(18*32+16) /* TSX Suspend Load Address Tracking */
 #define X86_FEATURE_PCONFIG		(18*32+18) /* Intel PCONFIG */
 #define X86_FEATURE_AVX512_FP16		(18*32+23) /* AVX-512 FP16 */
 #define X86_FEATURE_SPEC_CTRL		(18*32+26) /* "" Speculation Control (IBRS + IBPB) */
