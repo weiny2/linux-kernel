b632c4dced826cdb41337260874959a22763fae1
Ira Weiny <ira.weiny@intel.com>
2020-07-29 15:04:38 -0700

    drives/staging: Utilize new kmap_thread()
    These kmap() calls are localized to a single thread.  To avoid the over
    head of global PKRS updates use the new kmap_thread() call.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- drivers/staging/rts5208/rtsx_transport.c
+++ drivers/staging/rts5208/rtsx_transport.c
@@ -92,13 +92,13 @@ unsigned int rtsx_stor_access_xfer_buf(unsigned char *buffer,
 			while (sglen > 0) {
 				unsigned int plen = min(sglen, (unsigned int)
 						PAGE_SIZE - poff);
-				unsigned char *ptr = kmap(page);
+				unsigned char *ptr = kmap_thread(page);
 
 				if (dir == TO_XFER_BUF)
 					memcpy(ptr + poff, buffer + cnt, plen);
 				else
 					memcpy(buffer + cnt, ptr + poff, plen);
-				kunmap(page);
+				kunmap_thread(page);
 
 				/* Start at the beginning of the next page */
 				poff = 0;
