e4ae00f882ffecade327dfc91cc4b531488b2e1e
Ira Weiny <ira.weiny@intel.com>
2020-11-10 13:32:50 -0800

    fs/cifs: Convert to memcpy_page()
    Use memcpy_page() instead of open coding kmap/memcpy/kunmap.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- fs/cifs/smb2ops.c
+++ fs/cifs/smb2ops.c
@@ -4228,12 +4228,9 @@ smb3_init_transform_rq(struct TCP_Server_Info *server, int num_rqst,
 
 			rqst_page_get_length(&new_rq[i], j, &len, &offset);
 
-			dst = (char *) kmap(new_rq[i].rq_pages[j]) + offset;
-			src = (char *) kmap(old_rq[i - 1].rq_pages[j]) + offset;
-
-			memcpy(dst, src, len);
-			kunmap(new_rq[i].rq_pages[j]);
-			kunmap(old_rq[i - 1].rq_pages[j]);
+			memcpy_page(new_rq[i].rq_pages[j], offset,
+				    old_rq[i - 1].rq_pages[j], offset,
+				    len);
 		}
 	}
 
