a6b0a6d1f79f3658272e1b877a8f32da36c9246c
Ira Weiny <ira.weiny@intel.com>
2021-02-22 17:31:38 -0800

    kmap: Make kmap work for devmap protected pages
    Today, kmap_{local_page,atomic}() handle granting access to HIGHMEM
    pages without the caller needing to know if the page is HIGHMEM, or not.
    Use that existing infrastructure to grant access to PGMAP (PKS)
    protected pages.
    
    kmap_{local_page,atomic}() are both thread local mappings so they work
    well with the thread specific protections available within PKS.
    
    On the other hand, the kmap() call is not changed.  kmap() allows for a
    mapping to be shared with other threads, while PKS protections operate
    on a thread local basis.  For this reason, and the desire to move away
    from mappings like this, kmap() is left unsupported.
    
    This behavior is safe because neither of the 2 current DAX-capable
    filesystems (ext4 and xfs) perform such global mappings.  And known
    device drivers that would handle devmap pages are not using kmap().  Any
    future filesystems that gain DAX support, or device drivers wanting to
    support devmap protected pages will need to use kmap_local_page().
    
    Note: HIGHMEM support is mutually exclusive with PGMAP protection.  The
    rationale is mainly to reduce complexity, but also because direct-map
    exposure is already mitigated by default on HIGHMEM systems because
    by definition HIGHMEM systems do not have large capacities of memory
    in the direct map.
    
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes for V10
    	Include memremap.h because of upstream rework
    
    Changes for V9
    	From Dan Williams
    		Update commit message
    			Clarify why kmap is not 'compatible' with PKS
    			Explain the HIGHMEM system exclusion more
    	Remove pgmap_protection_flag_invalid() from kmap
    	s/pks_mk*/pks_set*/
    
    Changes for V8
    	Reword commit message

--- include/linux/highmem-internal.h
+++ include/linux/highmem-internal.h
@@ -151,6 +151,8 @@ static inline void totalhigh_pages_add(long count)
 
 #else /* CONFIG_HIGHMEM */
 
+#include <linux/memremap.h>
+
 static inline struct page *kmap_to_page(void *addr)
 {
 	return virt_to_page(addr);
@@ -174,6 +176,7 @@ static inline void kunmap(struct page *page)
 
 static inline void *kmap_local_page(struct page *page)
 {
+	pgmap_set_readwrite(page);
 	return page_address(page);
 }
 
@@ -197,6 +200,7 @@ static inline void __kunmap_local(void *addr)
 #ifdef ARCH_HAS_FLUSH_ON_KUNMAP
 	kunmap_flush_on_unmap(addr);
 #endif
+	pgmap_set_noaccess(kmap_to_page(addr));
 }
 
 static inline void *kmap_atomic(struct page *page)
@@ -206,6 +210,7 @@ static inline void *kmap_atomic(struct page *page)
 	else
 		preempt_disable();
 	pagefault_disable();
+	pgmap_set_readwrite(page);
 	return page_address(page);
 }
 
@@ -224,6 +229,7 @@ static inline void __kunmap_atomic(void *addr)
 #ifdef ARCH_HAS_FLUSH_ON_KUNMAP
 	kunmap_flush_on_unmap(addr);
 #endif
+	pgmap_set_noaccess(kmap_to_page(addr));
 	pagefault_enable();
 	if (IS_ENABLED(CONFIG_PREEMPT_RT))
 		migrate_enable();
