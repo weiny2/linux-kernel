ede245528b37bcce60d74a16ab13c16cdaa48533
Ira Weiny <ira.weiny@intel.com>
2021-11-15 12:38:24 -0800

    mm/pkeys: Introduce pks_set_noaccess()
    After a valid access consumers will want to change PKS protections back
    to No Access for their pkey.
    
    Define pks_set_noaccess() to update the specified pkey.
    
    Add documentation.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes for V9
    	Move to pks.h
    	Change pkey type to u8
    	From 0day
    		Fix documentation link
    	From Dave Hansen
    		use pkey
    		s/pks_mk*/pks_set*/
    
    Changes for V8
    	Make the call inline
    	Split this patch out from 'Add PKS kernel API'
    	Include documentation in this patch

--- include/linux/pks.h
+++ include/linux/pks.h
@@ -11,6 +11,18 @@
 void pks_update_protection(u8 pkey, u8 protection);
 
 /**
+ * pks_set_noaccess() - Disable all access to the domain
+ * @pkey: the pkey for which the access should change.
+ *
+ * Disable all access to the domain specified by pkey.  This is not a global
+ * update and only affects the current running thread.
+ */
+static inline void pks_set_noaccess(u8 pkey)
+{
+	pks_update_protection(pkey, PKEY_DISABLE_ACCESS);
+}
+
+/**
  * pks_set_readwrite() - Make the domain Read/Write
  * @pkey: the pkey for which the access should change.
  *
@@ -24,6 +36,7 @@ static inline void pks_set_readwrite(u8 pkey)
 
 #else /* !CONFIG_ARCH_ENABLE_SUPERVISOR_PKEYS */
 
+static inline void pks_set_noaccess(u8 pkey) {}
 static inline void pks_set_readwrite(u8 pkey) {}
 
 #endif /* CONFIG_ARCH_ENABLE_SUPERVISOR_PKEYS */
