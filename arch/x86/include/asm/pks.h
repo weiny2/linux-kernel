5870802b38ffc5140e62a7a1bfe77c6de5492c37
Ira Weiny <ira.weiny@intel.com>
2021-12-08 16:32:35 -0800

    x86/pkeys: Introduce pks_write_pkrs()
    Writing to MSR's is inefficient.  Even though the underlying PKS
    register, MSR_IA32_PKRS, is not serializing; writing to the MSR should
    be avoided if possible.  Especially when updates are made in critical
    paths such as the scheduler or the entry code.
    
    Introduce pks_write_pkrs().  pks_write_pkrs() avoids writing
    MSR_IA32_PKRS if the pkrs value has not changed for the current CPU.
    Most of the callers are in a non-preemptable code path.  Therefore,
    avoid calling preempt_{disable,enable}() to protect the per-cpu cache
    and instead rely on outer calls for this protection.  Do the same with
    checks to X86_FEATURE_PKS.
    
    pks_write_pkrs() is made inline to facilitate updating the MSR by thread
    code as efficiently as possible.  This also improves security by
    removing pks_write_pkrs() as a function entry point.
    
    On startup, while unlikely, the PKS_INIT_VALUE may be 0.  This would
    prevent pks_write_pkrs() from updating the MSR because of the initial
    value of the per-cpu cache.  Therefore, keep the MSR write in
    pks_setup() to ensure the MSR is initialized at least one time.
    
    Suggested-by: Dave Hansen <dave.hansen@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes for V11
    	From Kees Cook
    		Make pks_write_pkrs() inline
    	Make new_pkrs parameter const
    
    Changes for V9
    	From Dave Hansen
    		Update commit message with a bit more detail about why
    			this optimization is needed
    		Update the code comments as well.
    
    Changes for V8
    	From Thomas
    		Remove get/put_cpu_ptr() and make this a 'lower level
    		call.  This makes it preemption unsafe but it is called
    		mostly where preemption is already disabled.  Add this
    		as a predicate of the call and those calls which need to
    		can disable preemption.
    		Add lockdep assert for preemption
    	Ensure MSR gets written even if the PKS_INIT_VALUE is 0.
    	Completely re-write the commit message.
    	s/write_pkrs/pks_write_pkrs/
    	Split this off into a singular patch
    
    Changes for V7
    	Create a dynamic pkrs_initial_value in early init code.
    	Clean up comments
    	Add comment to macro guard

--- arch/x86/include/asm/pks.h
+++ arch/x86/include/asm/pks.h
@@ -4,7 +4,40 @@
 
 #ifdef CONFIG_ARCH_ENABLE_SUPERVISOR_PKEYS
 
+DECLARE_PER_CPU(u32, pkrs_cache);
+
 void pks_setup(void);
+
+/*
+ * pks_write_pkrs() - Write the pkrs of the current CPU
+ * @new_pkrs: New value to write to the current CPU register
+ *
+ * Optimizes the MSR writes by maintaining a per cpu cache.
+ *
+ * Context: must be called with preemption disabled
+ * Context: must only be called if PKS is enabled
+ *
+ * It should also be noted that the underlying WRMSR(MSR_IA32_PKRS) is not
+ * serializing but still maintains ordering properties similar to WRPKRU.
+ * The current SDM section on PKRS needs updating but should be the same as
+ * that of WRPKRU.  Quote from the WRPKRU text:
+ *
+ *     WRPKRU will never execute transiently. Memory accesses
+ *     affected by PKRU register will not execute (even transiently)
+ *     until all prior executions of WRPKRU have completed execution
+ *     and updated the PKRU register.
+ */
+static inline void pks_write_pkrs(const u32 new_pkrs)
+{
+	u32 pkrs = __this_cpu_read(pkrs_cache);
+
+	lockdep_assert_preemption_disabled();
+
+	if (pkrs != new_pkrs) {
+		__this_cpu_write(pkrs_cache, new_pkrs);
+		wrmsrl(MSR_IA32_PKRS, new_pkrs);
+	}
+}
 
 #else /* !CONFIG_ARCH_ENABLE_SUPERVISOR_PKEYS */
 
