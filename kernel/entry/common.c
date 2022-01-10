83e683311e5fefb29b1d2af83f733f9f6a234e07
Ira Weiny <ira.weiny@intel.com>
2022-01-10 15:06:07 -0800

    entry: Add calls for save/restore auxiliary pt_regs
    Some architectures have auxiliary pt_regs space which is available to
    store extra information on the stack.  For ease of implementation the
    common C code was left to fill in the data when needed.
    
    Add calls to the architecture save and restore auxiliary pt_regs
    functions.  Define empty calls for any architecture which does not have
    auxiliary pt_regs.
    
    NOTE: Due to the split nature of the Xen exit code
    irqentry_exit_cond_resched() requires an unbalanced call to
    arch_restore_aux_pt_regs().
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes for V9
    	Update commit message
    
    Changes for V8
    	New patch which introduces a generic auxiliary pt_register save
    		restore.

--- kernel/entry/common.c
+++ kernel/entry/common.c
@@ -320,7 +320,7 @@ noinstr irqentry_state_t irqentry_enter(struct pt_regs *regs)
 
 	if (user_mode(regs)) {
 		irqentry_enter_from_user_mode(regs);
-		return ret;
+		goto aux_save;
 	}
 
 	/*
@@ -360,7 +360,7 @@ noinstr irqentry_state_t irqentry_enter(struct pt_regs *regs)
 		instrumentation_end();
 
 		ret.exit_rcu = true;
-		return ret;
+		goto aux_save;
 	}
 
 	/*
@@ -374,6 +374,11 @@ noinstr irqentry_state_t irqentry_enter(struct pt_regs *regs)
 	kmsan_unpoison_entry_regs(regs);
 	rcu_irq_enter_check_tick();
 	trace_hardirqs_off_finish();
+	instrumentation_end();
+
+aux_save:
+	instrumentation_begin();
+	arch_save_aux_pt_regs(regs);
 	instrumentation_end();
 
 	return ret;
@@ -406,12 +411,17 @@ void dynamic_irqentry_exit_cond_resched(void)
 
 void irqentry_exit_cond_resched(struct pt_regs *regs)
 {
+	arch_restore_aux_pt_regs(regs);
 	irqentry_exit_cond_resched_internal();
 }
 
 noinstr void irqentry_exit(struct pt_regs *regs, irqentry_state_t state)
 {
 	lockdep_assert_irqs_disabled();
+
+	instrumentation_begin();
+	arch_restore_aux_pt_regs(regs);
+	instrumentation_end();
 
 	/* Check whether this returns to user mode */
 	if (user_mode(regs)) {
@@ -465,6 +475,7 @@ irqentry_state_t noinstr irqentry_nmi_enter(struct pt_regs *regs)
 	kmsan_unpoison_entry_regs(regs);
 	trace_hardirqs_off_finish();
 	ftrace_nmi_enter();
+	arch_save_aux_pt_regs(regs);
 	instrumentation_end();
 
 	return irq_state;
@@ -473,6 +484,7 @@ irqentry_state_t noinstr irqentry_nmi_enter(struct pt_regs *regs)
 void noinstr irqentry_nmi_exit(struct pt_regs *regs, irqentry_state_t irq_state)
 {
 	instrumentation_begin();
+	arch_restore_aux_pt_regs(regs);
 	ftrace_nmi_exit();
 	if (irq_state.lockdep) {
 		trace_hardirqs_on_prepare();
