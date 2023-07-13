3394ce85e912b328886077c5a439c65284767bb8
Ira Weiny <ira.weiny@intel.com>
2023-07-13 09:24:52 -0700

    firmware/efi: Process CXL Component Events
    BIOS can configure memory devices as firmware first.  This will send CXL
    events to the firmware instead of the OS.  The firmware can then send
    these events to the OS via UEFI.
    
    UEFI v2.10 section N.2.14 defines a Common Platform Error Record (CPER)
    format for CXL Component Events.  The format is mostly the same as the
    CXL Common Event Record Format.  The difference is the use of a GUID in
    the Section Type rather than a UUID as part of the event itself.
    
    Add EFI support to detect CXL CPER records and call a registered
    notifier with the event.  Enforce that only one notifier call can be
    registered at any time.
    
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes for v3:
    [djbw: check notifier vs registered and error if not the same]
    [djbw: check for previous notifier and fail if already set]
    [djbw: use guard for rw sem]
    [djbw: pass the event type and record data as 2 parameters]
    [djbw: prefer __packed over pragma packed]
    [djbw: clean up commit message]
    [iweiny: clean structure names]

--- drivers/firmware/efi/cper_cxl.h
+++ drivers/firmware/efi/cper_cxl.h
@@ -10,10 +10,37 @@
 #ifndef LINUX_CPER_CXL_H
 #define LINUX_CPER_CXL_H
 
+#include <linux/cxl-event.h>
+
 /* CXL Protocol Error Section */
 #define CPER_SEC_CXL_PROT_ERR						\
 	GUID_INIT(0x80B9EFB4, 0x52B5, 0x4DE3, 0xA7, 0x77, 0x68, 0x78,	\
 		  0x4B, 0x77, 0x10, 0x48)
+
+/* CXL Event record UUIDs are formated at GUIDs and reported in section type */
+/*
+ * General Media Event Record
+ * CXL rev 3.0 Section 8.2.9.2.1.1; Table 8-43
+ */
+#define CPER_SEC_CXL_GEN_MEDIA_GUID					\
+	GUID_INIT(0xfbcd0a77, 0xc260, 0x417f,				\
+		  0x85, 0xa9, 0x08, 0x8b, 0x16, 0x21, 0xeb, 0xa6)
+
+/*
+ * DRAM Event Record
+ * CXL rev 3.0 section 8.2.9.2.1.2; Table 8-44
+ */
+#define CPER_SEC_CXL_DRAM_GUID						\
+	GUID_INIT(0x601dcbb3, 0x9c06, 0x4eab,				\
+		  0xb8, 0xaf, 0x4e, 0x9b, 0xfb, 0x5c, 0x96, 0x24)
+
+/*
+ * Memory Module Event Record
+ * CXL rev 3.0 section 8.2.9.2.1.3; Table 8-45
+ */
+#define CPER_SEC_CXL_MEM_MODULE_GUID					\
+	GUID_INIT(0xfe927475, 0xdd59, 0x4339,				\
+		  0xa5, 0x86, 0x79, 0xba, 0xb1, 0x13, 0xb7, 0x74)
 
 #pragma pack(1)
 
@@ -62,5 +89,7 @@ struct cper_sec_prot_err {
 #pragma pack()
 
 void cper_print_prot_err(const char *pfx, const struct cper_sec_prot_err *prot_err);
+void cxl_cper_post_event(const char *pfx, guid_t *sec_type,
+			 struct cxl_cper_event_rec *rec);
 
 #endif //__CPER_CXL_
