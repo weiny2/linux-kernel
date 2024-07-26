cad6b0b760a04359a6ae0deafe0e926f057373a5
Navneet Singh <navneet.singh@intel.com>
2024-07-26 13:18:11 -0500

    dax/region: Create resources on sparse DAX regions
    DAX regions which map dynamic capacity partitions require that memory be
    allowed to come and go.  Recall sparse regions were created for this
    purpose.  Now that extents can be realized within DAX regions the DAX
    region driver can start tracking sub-resource information.
    
    The tight relationship between DAX region operations and extent
    operations require memory changes to be controlled synchronously with
    the user of the region.  Synchronize through the dax_region_rwsem and by
    having the region driver drive both the region device as well as the
    extent sub-devices.
    
    Recall requests to remove extents can happen at any time and that a host
    is not obligated to release the memory until it is not being used.  If
    an extent is not used allow a release response.
    
    The DAX layer has no need for the details of the CXL memory extent
    devices.  Expose extents to the DAX layer as device children of the DAX
    region device.  A single callback from the driver aids the DAX layer to
    determine if the child device is an extent.  The DAX layer also
    registers a devres function to automatically clean up when the device is
    removed from the region.
    
    There is a race between extents being surfaced and the dax_cxl driver
    being loaded.  The driver must therefore scan for any existing extents
    while still under the device lock.
    
    Respond to extent notifications.  Manage the DAX region resource tree
    based on the extents lifetime.  Return the status of remove
    notifications to lower layers such that it can manage the hardware
    appropriately.
    
    Signed-off-by: Navneet Singh <navneet.singh@intel.com>
    Co-developed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes:
    [iweiny: patch reorder]
    [iweiny: move hunks from other patches to clarify code changes and
             add/release flows WRT dax regions]
    [iweiny: use %par]
    [iweiny: clean up variable names]
    [iweiny: Simplify sparse_ops]
    [Fan: avoid open coding range_len()]
    [djbw: s/reg_ext/region_extent]

--- drivers/dax/cxl.c
+++ drivers/dax/cxl.c
@@ -5,6 +5,60 @@
 
 #include "../cxl/cxl.h"
 #include "bus.h"
+#include "dax-private.h"
+
+static int __cxl_dax_add_resource(struct dax_region *dax_region,
+				  struct region_extent *region_extent)
+{
+	resource_size_t start, length;
+	struct device *dev;
+
+	dev = &region_extent->dev;
+	start = dax_region->res.start + region_extent->hpa_range.start;
+	length = range_len(&region_extent->hpa_range);
+	return dax_region_add_resource(dax_region, dev, start, length);
+}
+
+static int cxl_dax_add_resource(struct device *dev, void *data)
+{
+	struct dax_region *dax_region = data;
+	struct region_extent *region_extent;
+
+	region_extent = to_region_extent(dev);
+	if (!region_extent)
+		return 0;
+
+	dev_dbg(dax_region->dev, "Adding resource HPA %par\n",
+		&region_extent->hpa_range);
+
+	return __cxl_dax_add_resource(dax_region, region_extent);
+}
+
+static int cxl_dax_region_notify(struct device *dev,
+				 struct cxl_notify_data *notify_data)
+{
+	struct cxl_dax_region *cxlr_dax = to_cxl_dax_region(dev);
+	struct dax_region *dax_region = dev_get_drvdata(dev);
+	struct region_extent *region_extent = notify_data->region_extent;
+
+	switch (notify_data->event) {
+	case DCD_ADD_CAPACITY:
+		return __cxl_dax_add_resource(dax_region, region_extent);
+	case DCD_RELEASE_CAPACITY:
+		return dax_region_rm_resource(dax_region, &region_extent->dev);
+	case DCD_FORCED_CAPACITY_RELEASE:
+	default:
+		dev_err(&cxlr_dax->dev, "Unknown DC event %d\n",
+			notify_data->event);
+		break;
+	}
+
+	return -ENXIO;
+}
+
+struct dax_sparse_ops sparse_ops = {
+	.is_extent = is_region_extent,
+};
 
 static int cxl_dax_region_probe(struct device *dev)
 {
@@ -24,14 +78,16 @@ static int cxl_dax_region_probe(struct device *dev)
 		flags |= IORESOURCE_DAX_SPARSE_CAP;
 
 	dax_region = alloc_dax_region(dev, cxlr->id, &cxlr_dax->hpa_range, nid,
-				      PMD_SIZE, flags);
+				      PMD_SIZE, flags, &sparse_ops);
 	if (!dax_region)
 		return -ENOMEM;
 
-	if (cxlr->mode == CXL_REGION_DC)
+	if (cxlr->mode == CXL_REGION_DC) {
+		device_for_each_child(&cxlr_dax->dev, dax_region,
+				      cxl_dax_add_resource);
 		/* Add empty seed dax device */
 		dev_size = 0;
-	else
+	} else
 		dev_size = range_len(&cxlr_dax->hpa_range);
 
 	data = (struct dev_dax_data) {
@@ -47,6 +103,7 @@ static int cxl_dax_region_probe(struct device *dev)
 static struct cxl_driver cxl_dax_region_driver = {
 	.name = "cxl_dax_region",
 	.probe = cxl_dax_region_probe,
+	.notify = cxl_dax_region_notify,
 	.id = CXL_DEVICE_DAX_REGION,
 	.drv = {
 		.suppress_bind_attrs = true,
